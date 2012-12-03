ced0addd3349a5d83ab34a860fc9edf56fb80c71
Ats Nisov <ats.nisov@googlemail.com>
2012-12-03 16:40:17 +0200

    New feature: Added a button to staff's submission inspection view, which enables assistants and teachers to re-submit the respective submission to the assessment service.

--- exercise/urls.py
+++ exercise/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 from exercise.views import view_submission, view_exercise, view_submitted_file
 from exercise.staff_views import list_exercise_submissions, inspect_exercise_submission, \
-    add_or_edit_exercise, fetch_exercise_metadata, submission_assessment
+    add_or_edit_exercise, fetch_exercise_metadata, submission_assessment, re_submit_to_service
 from exercise.async_views import new_async_submission, grade_async_submission
 
 urlpatterns = patterns('',
@@ -16,4 +16,5 @@ urlpatterns = patterns('',
     (r'^manage/(?P<module_id>\d+)/(?P<exercise_id>\d+)/$', add_or_edit_exercise),
     (r'^rest/exercise/(?P<exercise_id>\d+)/students/(?P<student_ids>[\d\-]+)/(?P<hash>\w+)/$', new_async_submission),
     (r'^rest/submission/(?P<submission_id>\d+)/(?P<hash>\w+)/$', grade_async_submission),
+    (r'^submissions/re-submit-to-service/(?P<submission_id>\d+)/$', re_submit_to_service),
 )
