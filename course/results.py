ce173fe86702ae85394edcf26e094f9a38f67317
Ats Nisov <ats.nisov@googlemail.com>
2013-01-03 16:02:29 +0200

    typo fix

--- course/results.py
+++ course/results.py
@@ -35,7 +35,7 @@ class ResultTable:
         self.best_submissions = Submission.objects.filter(
             exercise__course_module__course_instance=course_instance).exclude(
             submitters=None).values("submitters", "exercise").annotate(
-            best=Max("grade")) .order_by()
+            best=Max("grade")).order_by()
 
         # self.best_submissions elements only contain the id of the exercise.
         # We need other exercise data too so we fetch the related BaseExercise
