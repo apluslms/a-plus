759691c3e8a05eebbe05c5b353553d56f1293c92
Ats Nisov <ats.nisov@googlemail.com>
2013-01-25 15:20:30 +0200

    Some quick code to get EmbeddedTabs to work. This is mostly throw-away.

--- apps/admin.py
+++ apps/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from models import *
 
 
+class EmbeddedTabAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(EmbeddedTab, EmbeddedTabAdmin)
+
+
 class RSSPluginAdmin(admin.ModelAdmin):
     pass
 admin.site.register(RSSPlugin, RSSPluginAdmin)
