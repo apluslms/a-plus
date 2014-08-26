bfaeb69ae13d5f54b2025ef8f8463ca62ba62264
LassiHaaranen <lassi.haaranen@aalto.fi>
2014-08-26 12:40:06 +0300

    Added example exercise that has file attachments

--- doc/example_grader.py
+++ doc/example_grader.py
@@ -1,4 +1,4 @@
-import SimpleHTTPServer, SocketServer, cgi
+import SimpleHTTPServer, SocketServer
 from urlparse import parse_qs
 
 PORT = 8888
@@ -16,32 +16,60 @@ class ExerciseGrader(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
   # On GET-request return the exercise 
   def do_GET(self):
-    response = open('first_exercise.html','r').read() 
-    self.send_response(200)
-    self.send_header('Content-type','text/html')    
-    self.send_header("Content-Length", len(response))
-    self.end_headers()
-    self.wfile.write(response)
+
+    # Simple exercise
+    if "/first_exercise/" in self.path:
+      response = open('first_exercise.html','r').read()     
+      self.send_response(200)
+      self.send_header('Content-type','text/html')    
+      self.send_header("Content-Length", len(response))
+      self.end_headers()
+      self.wfile.write(response)
+    # Attachment exercise 
+    elif "/attachment_exercise/" in self.path:
+      response = open('attachment_exercise.html','r').read()     
+      self.send_response(200)
+      self.send_header('Content-type','text/html')    
+      self.send_header("Content-Length", len(response))
+      self.end_headers()
+      self.wfile.write(response)
 
   # On POSTs get the answer, grade it and return the results
   def do_POST(self):
     length = int(self.headers.getheader('content-length'))
     postvars = parse_qs(self.rfile.read(length), keep_blank_values=1)
-    points,max_points = grade_first_ex(postvars['answer'][0])
 
-    response = '<html><head>\n' +\
-                '<meta name="points" value="' + str(points) +'" />\n' +\
-                '<meta name="max-points" value="' + str(max_points) + '" />\n' +\
-                '<meta name="status" value="graded" />\n' + \
-                '</head><body>' +\
-                'Submission succesful,  you got ' + str(points) + '/' +\
-                str(max_points)+ ' points!</body></html>'
+    # Simple exercise
+    if "/first_exercise/" in self.path:
+      points,max_points = grade_first_ex(postvars['answer'][0])
 
-    self.send_response(200)
-    self.send_header('Content-type','text/html')    
-    self.send_header("Content-Length", len(response))
-    self.end_headers()
-    self.wfile.write(response)
+      response = '<html><head>\n' +\
+                  '<meta name="points" value="' + str(points) +'" />\n' +\
+                  '<meta name="max-points" value="' + str(max_points) + '" />\n' +\
+                  '<meta name="status" value="graded" />\n' + \
+                  '</head><body>' +\
+                  'Submission succesful,  you got ' + str(points) + '/' +\
+                  str(max_points)+ ' points!</body></html>'
+      self.send_response(200)
+      self.send_header('Content-type','text/html')    
+      self.send_header("Content-Length", len(response))
+      self.end_headers()
+      self.wfile.write(response)
+
+    # Attachment exercise. Note that the file is only stored in A+ 
+    if "/attachment_exercise/" in self.path:
+      response = '<html><head>\n' +\
+                  '<meta name="points" value="0" />\n' +\
+                  '<meta name="max-points" value="100" />\n' +\
+                  '<meta name="status" value="waiting" />\n' + \
+                  '</head><body>' +\
+                  'Submission stored, you will be notified when it is assessed.</body></html>'
+
+      self.send_response(200)
+      self.send_header('Content-type','text/html')    
+      self.send_header("Content-Length", len(response))
+      self.end_headers()
+      self.wfile.write(response)
 
 httpd = SocketServer.TCPServer(("", PORT), ExerciseGrader)
 print "Serving at port:", PORT
