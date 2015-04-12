5a3d5082902bd2362ee1e317c9e87d74eb72c6d6
Antti Tikka <antti.tikka@trimico.fi>
2015-04-12 20:41:05 +0300

    Added test instance password for selenium tests

--- selenium/login_test.py
+++ selenium/login_test.py
@@ -7,7 +7,7 @@ from page_objects import LoginPage
 
 class LoginTest(unittest.TestCase):
     username = "root"
-    password = "root"
+    password = "maesh3Reem"
 
     def setUp(self):
         # Set up browser logging
@@ -18,10 +18,16 @@ class LoginTest(unittest.TestCase):
     def testLoginToTestCourseInstance(self):
         login_page = LoginPage(self.driver)
         login_page.login_to_course("APLUS", self.username, self.password)
+        login_page.logout()
+
 
     def testLoginToExampleHookCourseInstance(self):
         login_page = LoginPage(self.driver)
         login_page.login_to_course("HOOK", self.username, self.password)
+        login_page.logout()
 
     def tearDown(self):
         self.driver.close()
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
