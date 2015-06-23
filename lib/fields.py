837fa866ee4ff1e73f97e592e795943256403656
Teemu Lehtinen <teemu.t.lehtinen@aalto.fi>
2015-06-23 14:30:32 +0300

    Rewrite fields for modern Django.

--- lib/fields.py
+++ lib/fields.py
@@ -1,111 +1,59 @@
 import json
 
-from django.db import models
 from django.core import exceptions
-from django.core.serializers.json import DjangoJSONEncoder
+from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.forms.fields import Field
-from django.forms.util import ValidationError as FormValidationError
 
-########################################################################
-# Adapted from http://djangosnippets.org/snippets/1914/
-########################################################################
-class PercentField(models.FloatField):
-    """ A field that allows a value between 0 and 1 """
-
-    default_error_messages = {
-        'range': 'The number must be between 0.0 and 1.0.',
-    }
-
-    def clean(self, value, model_instance):
-        """
-        Validates that the input can be converted to a value between 0 and 1.
-        Returns a float
-        """
-        value = super(PercentField, self).clean(value, model_instance)
-
-        if value is None:
-            return None
-
-        if (0.0 <= value <= 1.0):
-            return value
-
-        raise exceptions.ValidationError(self.error_messages['range'])
 
 try:
     # If South is used, it requires additional rules for processing
     # custom fields. In this case we add empty inspection rules for the
-    # percentage field.
+    # percent field.
     from south.modelsinspector import add_introspection_rules
     add_introspection_rules([], ["^lib\.fields\.PercentField"])
 except:
     pass
 
-########################################################################
-# Django-JSONField by Brad Jasper
-# https://github.com/bradjasper/django-jsonfield
-########################################################################
-class JSONFormField(Field):
-    def clean(self, value):
-        if not value and not self.required:
-            return None
 
-        value = super(JSONFormField, self).clean(value)
-
-        try:
-            json.loads(value)
-        except ValueError:
-            raise FormValidationError(_("Enter valid JSON"))
+class PercentField(models.FloatField):
+    """
+    A float in range 0.0 to 1.0
+    """
+    def clean(self, value, model_instance):
+        value = super(PercentField, self).clean(value, model_instance)
+        if value and (value < 0.0 or value > 1.0):
+            raise exceptions.ValidationError(
+                _("The number must be between 0.0 and 1.0")
+            )
         return value
 
-class JSONField(models.TextField, metaclass=models.SubfieldBase):
-    """JSONField is a generic textfield that serializes/unserializes JSON objects"""
 
+class JSONField(models.TextField):
+    """
+    Stores JSON object in a text field.
+    """
     def __init__(self, *args, **kwargs):
-        self.dump_kwargs = kwargs.pop('dump_kwargs', {'cls': DjangoJSONEncoder})
-        self.load_kwargs = kwargs.pop('load_kwargs', {})
-
         super(JSONField, self).__init__(*args, **kwargs)
 
     def to_python(self, value):
-        """Convert string value to JSON"""
-        if isinstance(value, str):
-            try:
-                return json.loads(value, **self.load_kwargs)
-            except ValueError:
-                pass
-        return value
+        if not value:
+            return None
+        try:
+            return json.loads(value)
+        except (TypeError, ValueError):
+            raise exceptions.ValidationError(
+                _("Enter valid JSON.")
+            )
 
-    def get_db_prep_value(self, value, connection, prepared=False):
-        """Convert JSON object to a string"""
-
+    def get_prep_value(self, value):
+        if not value:
+            return ""
         if isinstance(value, str):
             return value
-        return json.dumps(value, **self.dump_kwargs)
-
-    def value_to_string(self, obj):
-        value = self._get_val_from_obj(obj)
-        return self.get_prep_value(value)
-
-    def value_from_object(self, obj):
-        return json.dumps(super(JSONField, self).value_from_object(obj))
+        return json.dumps(value)
 
     def formfield(self, **kwargs):
-        if "form_class" not in kwargs:
-            kwargs["form_class"] = JSONFormField
-
-        field = super(JSONField, self).formfield(**kwargs)
-
-        if not field.help_text:
-            field.help_text = "Enter valid JSON"
-
-        return field
-
-try:
-    from south.modelsinspector import add_introspection_rules
-    add_introspection_rules([([JSONField], [], {
-        "dump_kwargs": ("dump_kwargs", {"default": {'cls': DjangoJSONEncoder}}),
-        "load_kwargs": ("load_kwargs", {"default": {}})
-    })], ["^lib\.fields\.JSONField"])
-except:
-    pass
+        print(kwargs)
+        defaults = { 'help_text': _("Enter valid JSON.") }
+        defaults.update(kwargs)
+        return super(JSONField, self).formfield(**defaults)
--- lib/helpers.py
+++ lib/helpers.py
@@ -15,11 +15,13 @@ def extract_form_errors(form):
 
 
 def get_random_string(length=32):
-    """ This function creates a random string with a given length.
-        The strings consist of upper and lower case letters and numbers.
-        @param length: the length of the randomized string, defaults to 32
-        @return: a random string containing lower and upper case letters and digits
-        """
+    """
+    This function creates a random string with a given length.
+    The strings consist of upper and lower case letters and numbers.
+        
+    @param length: the length of the randomized string, defaults to 32
+    @return: a random string containing lower and upper case letters and digits
+    """
 
     # Use all letters and numbers in the identifier
     choices = string.ascii_letters + string.digits
@@ -28,14 +30,15 @@ def get_random_string(length=32):
 
 
 def query_dict_to_list_of_tuples(query_dict):
-    """ This helper function creates a list of tuples with the values
-        from a QueryDict object. In a QueryDict the same key can have
-        several values, which is not possible with a typical dict nor a JSON
-        object. The resulting list will be similar to [(key1, value1), (key2, value2)].
+    """
+    This helper function creates a list of tuples with the values
+    from a QueryDict object. In a QueryDict the same key can have
+    several values, which is not possible with a typical dict nor a JSON
+    object. The resulting list will be similar to [(key1, value1), (key2, value2)].
 
-        @param query_dict: a QueryDict object
-        @return: a list of tuples with the same keys and values as in the given QueryDict
-        """
+    @param query_dict: a QueryDict object
+    @return: a list of tuples with the same keys and values as in the given QueryDict
+    """
     list_of_tuples = []
     for key in query_dict:
         for val in query_dict.getlist(key):
@@ -44,6 +47,9 @@ def query_dict_to_list_of_tuples(query_dict):
 
 
 def update_url_params(url, params):
+    """
+    Updates query parameters in an URL.
+    """
     url_parts = list(urllib.parse.urlparse(url))
     query = dict(urllib.parse.parse_qs(url_parts[4]))
     query.update(params)
