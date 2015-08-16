0b5335a132a745a4d1ce7eecc76c484ba583042e
Teemu Lehtinen <teemu.t.lehtinen@aalto.fi>
2015-08-16 12:06:45 +0300

    Unify course/exercise key syntax.

--- access/management/commands/exercises.py
+++ access/management/commands/exercises.py
@@ -2,31 +2,34 @@ from django.core.management.base import BaseCommand, CommandError
 from access.views import config
 
 class Command(BaseCommand):
-    args = "<course_key <exercise_key>>"
+    args = "<course_key</exercise_key>>"
     help = "Tests configuration files syntax."
-    
+
     def handle(self, *args, **options):
 
         # Check by arguments.
         if len(args) > 0:
-            course_key = args[0]
+            if "/" in args[0]:
+                course_key, exercise_key = args[0].split("/", 1)
+            else:
+                course_key = args[0]
+                exercise_key = None
             course = config.course_entry(course_key)
             if course is None:
                 raise CommandError("Course not found for key: %s" % (course_key))
             self.stdout.write("Configuration syntax ok for: %s" % (course_key))
-            
-            if len(args) > 1:
-                exercise_key = args[1]
+
+            if exercise_key:
                 (_course, exercise) = config.exercise_entry(course_key, exercise_key)
                 if exercise is None:
                     raise CommandError("Exercise not found for key: %s/%s" % (course_key, exercise_key))
                 self.stdout.write("Configuration syntax ok for: %s/%s" % (course_key, exercise_key))
-            
+
             else:
                 (_course, exercises) = config.exercises(course_key)
                 for exercise in exercises:
                     self.stdout.write("Configuration syntax ok for: %s/%s" % (course_key, exercise["key"]))
-        
+
         # Check all.
         else:
             for course in config.courses():
--- access/management/commands/grade.py
+++ access/management/commands/grade.py
@@ -6,23 +6,22 @@ from util.templates import template_to_str
 import os, shutil
 
 class Command(BaseCommand):
-    args = "course_key exercise_key <submit_file submit_file ...>"
+    args = "course_key/exercise_key <submit_file submit_file ...>"
     help = "Grades a given exercise submission as it would be in the grading queue."
-    
+
     def handle(self, *args, **options):
-        
+
         config = ConfigParser()
-        
+
         # Check arguments.
-        if len(args) < 2:
-            raise CommandError("Required arguments missing: course_key exercise_key")
-        course_key = args[0]
-        exercise_key = args[1]
-        
+        if len(args) < 1 or "/" not in args[0]:
+            raise CommandError("Required arguments missing: course_key/exercise_key")
+        course_key, exercise_key = args[0].split("/", 1)
+
         # Get exercise configuration.
         (course, exercise) = config.exercise_entry(course_key, exercise_key)
         if course is None:
-            raise CommandError("Course not found for key: %s" % (course_key))    
+            raise CommandError("Course not found for key: %s" % (course_key))
         if exercise is None:
             raise CommandError("Exercise not found for key: %s/%s" % (course_key, exercise_key))
         self.stdout.write('Exercise configuration retrieved.')
@@ -30,28 +29,28 @@ class Command(BaseCommand):
         # Check exercise type.
         if not "actions" in exercise:
             raise CommandError("Cannot grade: exercise does not configure asynchronous actions")
-        
+
         # Create submission.
         sdir = create_submission_dir(course, exercise)
-        if len(args) == 2:
+        if len(args) == 1:
             os.makedirs(sdir + "/user")
-        for n in range(2, len(args)):
+        for n in range(1, len(args)):
             name = args[n]
-            
+
             # Copy individual files.
             if os.path.isfile(name):
                 submit_path = submission_file_path(sdir, os.path.basename(name))
                 shutil.copy2(name, submit_path)
-            
+
             # Copy a directory.
             elif os.path.isdir(name):
-                if len(args) != 3:
+                if len(args) != 2:
                     raise CommandError("Can only submit one directory or multiple files.")
                 shutil.copytree(name, sdir + "/user", True)
-            
+
             else:
                 raise CommandError("Submit file not found: %s" % (name))
-        
+
         # Run actions.
         r = runactions(course, exercise, sdir)
         self.stdout.write("Response body:")
