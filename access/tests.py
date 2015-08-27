d69b7aa1810a28f0a601e09a05615f02cd9002fe
Teemu Lehtinen <teemu.t.lehtinen@aalto.fi>
2015-08-27 18:10:29 +0300

    Fix tests.

--- access/tests.py
+++ access/tests.py
@@ -2,43 +2,33 @@
 '''
 This module holds unit tests. It has nothing to do with the grader tests.
 '''
-from django.test import TestCase
 from django.conf import settings
+from django.test import TestCase
+
 from access.config import ConfigParser
 from util.shell import invoke_script
 
+
 class ConfigTestCase(TestCase):
 
-
     def setUp(self):
-        '''
-        '''
         self.config = ConfigParser()
 
-
     def test_parsing(self):
-        '''
-        '''
         from access.config import get_rst_as_html
-        print(get_rst_as_html('A **foobar**.'))
-        return
+        self.assertEqual(get_rst_as_html('A **foobar**.'), '<p>A <strong>foobar</strong>.</p>\n')
         import re
         from access.config import iterate_kvp_with_dfs
         data = {
-            'title|18n': {'en': 'A Title', 'fi': 'Eräs otsikko'},
+            'title|i18n': {'en': 'A Title', 'fi': 'Eräs otsikko'},
             'text|rst': 'Some **fancy** text with ``links <http://google.com>`` and code like ``echo "moi"``.'
         }
-        rgx = re.compile(r'^(.+)\|(\w+)$')
-        for k, v, p in iterate_kvp_with_dfs(data, key_regex=rgx):
-            g = rgx.match(k).groups()
-            print('- %s: %s (%s)' % (k, v, g))
-        self.config._process_data(data)
-        print(data)
-
+        self.config._process_exercise_data({ "lang": "en" }, data)
+        self.assertEqual(data["en"]["text"], data["fi"]["text"])
+        self.assertEqual(data["en"]["title"], "A Title")
+        self.assertEqual(data["fi"]["title"], "Eräs otsikko")
 
     def test_loading(self):
-        '''
-        '''
         courses = self.config.courses()
         self.assertGreater(len(courses), 0, "No courses configured")
         course_key = courses[0]["key"]
@@ -50,11 +40,8 @@ class ConfigTestCase(TestCase):
         root = self.config._course_root(course_key)
         self.assertEqual(ptime, root["ptime"])
 
-
     def test_shell_invoke(self):
-        '''
-        '''
-        r = invoke_script("prepare.sh", {})
+        r = invoke_script(settings.PREPARE_SCRIPT, {})
         self.assertEqual(1, r["code"])
-        r = invoke_script("prepare.sh", { "dir": settings.SUBMISSION_PATH })
+        r = invoke_script(settings.PREPARE_SCRIPT, { "course_key": "foo", "dir": settings.SUBMISSION_PATH })
         self.assertEqual(0, r["code"])
