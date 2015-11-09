74a58cf3d01bb8b7870a59a9ec702b898e48ce79
Teemu Lehtinen <teemu.t.lehtinen@aalto.fi>
2015-11-09 15:48:19 +0200

    Restore the 3rd party repository for Python 3.4

--- scripts/sandbox_available/install-python-3.4.sh
+++ scripts/sandbox_available/install-python-3.4.sh
@@ -3,6 +3,7 @@
 # Installs Python 3.4.
 #
 if ! [ -x /usr/local/bin/python3.4 ]; then
+    sudo add-apt-repository ppa:fkrull/deadsnakes
     apt-get -q update
     apt-get -qy install python3.4 python3.4-dev
 fi
