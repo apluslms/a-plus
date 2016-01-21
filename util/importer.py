9d410dd3d1d26a82b80ad13614ed204bb8c45d8a
Teemu Lehtinen <teemu.t.lehtinen@aalto.fi>
2016-01-21 10:44:17 +0200

    Fix exception import

--- util/importer.py
+++ util/importer.py
@@ -1,7 +1,22 @@
 '''
 Tools to import modules by name.
 '''
+from django.core.exceptions import ImproperlyConfigured
 from django.utils.module_loading import import_by_path
+
+
+class ImportError(Exception):
+    '''
+    Failed to import.
+    '''
+    def __init__(self, value, error=None):
+        self.value = value
+        self.error = error
+
+    def __str__(self):
+        if self.error is not None:
+            return "%s: %s" % (repr(self.value), repr(self.error))
+        return repr(self.value)
 
 
 def import_named(course, path):
@@ -10,4 +25,4 @@ def import_named(course, path):
     try:
         return import_by_path(path)
     except ImproperlyConfigured as e:
-        raise ConfigError("Invalid function path in exercise configuration.", e)
+        raise ImportError("Invalid function path in exercise configuration.", e)
