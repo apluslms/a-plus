01d340cc8e16b16f1990b0ce76b8741f888a9a51
Teemu Lehtinen <teemu.t.lehtinen@aalto.fi>
2016-02-12 13:41:03 +0200

    Fix for current aplus urls

--- access/types/auth.py
+++ access/types/auth.py
@@ -9,9 +9,9 @@ def detect_user(request):
 
     # Check submission_url for A+ userid.
     if not user and "submission_url" in request.GET:
-        parts = request.GET["submission_url"].partition("/exercise/rest/")[2].split("/")
-        if len(parts) > 4 and parts[2] == "students":
-            return parts[3]
+        parts = request.GET["submission_url"].partition("/rest/exercise/")[2].split("/")
+        if len(parts) > 4 and parts[1] == "students":
+            return parts[2].split("-")[0]
 
     if user:
         return user.strip()
