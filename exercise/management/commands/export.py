8ca42a01608a52c7ff8080aa550cb359c3368f24
Teemu Lehtinen <teemu.t.lehtinen@aalto.fi>
2016-05-30 16:46:43 +0300

    Add different course level exports.

--- exercise/management/commands/export.py
+++ exercise/management/commands/export.py
@@ -1,21 +1,35 @@
+import json, base64
 from django.core.management.base import BaseCommand, CommandError
 
-from exercise.models import BaseExercise
+from course.models import CourseInstance
+from exercise.models import BaseExercise, LearningObjectDisplay
 
 
 class Command(BaseCommand):
-    args = 'exercise_id'
-    help = 'Exports submissions for an exercise.'
+    args = 'exercise/course/json/views id'
+    help = 'Exports submission data.'
 
     def handle(self, *args, **options):
-        if len(args) != 1:
-            raise CommandError('Missing exercise_id')
+        if len(args) != 2:
+            raise CommandError('Missing arguments: exercise/course/views id')
+        if args[0] == 'exercise':
+            self.export_exercise(args[1])
+        elif args[0] == 'course':
+            self.export_course(args[1])
+        elif args[0] == 'json':
+            self.export_json(args[1])
+        elif args[0] == 'views':
+            self.export_views(args[1])
+        else:
+            raise CommandError('Unknown argument!')
 
-        exercise = BaseExercise.objects.filter(id=args[0]).first()
+    def export_exercise(self, eid):
+        exercise = BaseExercise.objects.filter(id=eid).first()
         if not exercise:
             raise CommandError('Exercise not found.')
 
-        submissions = list(exercise.submissions.all())
+        students = [u['id'] for u in exercise.course_module.course_instance.students.values('id')]
+        submissions = [s for s in exercise.submissions.all() if s.submitters.first().id in students]
 
         fields = []
         for submission in submissions:
@@ -24,14 +38,16 @@ class Command(BaseCommand):
                     fields.append(key)
         fields = sorted(fields)
 
-        header = [ 'Time', 'Email', 'Status', 'Grade' ]
+        header = [ 'Time', 'UID', 'Email', 'Status', 'Grade' ]
         header += fields
-        print(','.join(header))
+        self.print_row(header)
 
         for submission in submissions:
+            profile = submission.submitters.first()
             data = [
                 str(submission.submission_time),
-                submission.submitters.first().user.email,
+                str(profile.id),
+                profile.user.email,
                 submission.status,
                 str(submission.grade),
             ]
@@ -40,4 +56,72 @@ class Command(BaseCommand):
                 for key, val in submission.submission_data:
                     values[fields.index(key)].append(val.replace('"','\''))
             data += ['"' + (';'.join(v)) + '"' for v in values]
-            print(','.join(data))
+            self.print_row(data)
+
+    def export_course(self, cid):
+        instance = CourseInstance.objects.filter(id=cid).first()
+        if not instance:
+            raise CommandError('Course instance not found.')
+        students = [u['id'] for u in instance.students.values('id')]
+
+        self.print_row([ 'Time', 'UID', 'Email', 'EID', 'Exercise', 'Status', 'Grade' ])
+
+        for exercise in BaseExercise.objects.filter(course_module__course_instance=instance).all():
+            submissions = [s for s in exercise.submissions.all() if s.submitters.first().id in students]
+            for submission in submissions:
+                profile = submission.submitters.first()
+                self.print_row([
+                    str(submission.submission_time),
+                    str(profile.id),
+                    profile.user.email,
+                    str(exercise.id),
+                    str(exercise),
+                    submission.status,
+                    str(submission.grade),
+                ])
+
+    def export_json(self, cid):
+        instance = CourseInstance.objects.filter(id=cid).first()
+        if not instance:
+            raise CommandError('Course instance not found.')
+        students = [u['id'] for u in instance.students.values('id')]
+
+        data = []
+        for exercise in BaseExercise.objects.filter(course_module__course_instance=instance).all():
+            submissions = [s for s in exercise.submissions.all() if s.submitters.first().id in students]
+            for submission in submissions:
+                profile = submission.submitters.first()
+                data.append({
+                    'Time': str(submission.submission_time),
+                    'UID': str(profile.id),
+                    'Email': profile.user.email,
+                    'EID': str(exercise.id),
+                    'Exercise': str(exercise),
+                    'Status': submission.status,
+                    'Grade': str(submission.grade),
+                    'Grading data': submission.grading_data or {},
+                })
+        print(json.dumps(data))
+
+    def export_views(self, cid):
+        instance = CourseInstance.objects.filter(id=cid).first()
+        if not instance:
+            raise CommandError('Course instance not found.')
+
+        students = [u['id'] for u in instance.students.values('id')]
+        displays = [d for d in LearningObjectDisplay.objects.prefetch_related('profile', 'learning_object').all() if d.profile.id in students]
+
+        self.print_row(['Time', 'UID', 'Email', 'EID', 'Exercise'])
+        for d in displays:
+            self.print_row([
+                str(d.timestamp),
+                str(d.profile.id),
+                d.profile.user.email,
+                str(d.learning_object.id),
+                str(d.learning_object),
+            ])
+
+    def print_row(self, fields, quote=False):
+        if quote:
+            fields = ['"{}"'.format(f.replace('"','\'')) for f in fields]
+        print(','.join(fields))
