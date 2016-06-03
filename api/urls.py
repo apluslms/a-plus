ccbe1f20c24bd89152173110025484cc10e31d13
Ville Vironmäki <ville.vironmaki@student.tut.fi>
2016-06-04 02:14:03 +0300

    kommentointia APIiin

--- api/urls.py
+++ api/urls.py
@@ -2,12 +2,24 @@ from userprofile import userprofile_api
 from exercise import exercise_api
 from django.conf.urls import url, patterns, include
 
-# REST Framework URLs
+# REST Framework URLs. The root of API, e.g. www.plus.cs.tut.fi/api
+# To get deeper, take a look at folders: exercise, userprofile
 urlpatterns = patterns('',
+    # api/userprofile: lists all userprofiles.
+    # GET is available for everyone
+    # POST is for admin
     url(r'^userprofile/$', userprofile_api.UserList.as_view()),
+
+    # e.g. api/userprofile/1: GET/PUT/PATCH/DELETE to single user
+    # GET is for everyone and others need to be authenticated
     url(r'^userprofile/(?P<pk>[0-9]+)/$', userprofile_api.UserDetail.as_view()),
 
+    # api/learningobject: GET list of exercises
     url(r'^learningobject/$', exercise_api.LearningObjectList.as_view()),
+
+    # api/submission: GET/POST a submission
+    # GET is for getting the result of SubmissionDetail
+    # POST is for making new SubmissionDetail
     url(r'^submission/$', exercise_api.SubmissionList.as_view()),
 )
 
