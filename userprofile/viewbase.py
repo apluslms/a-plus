41cade91c665f2c6abd5029ffc6c08a728609544
Ville Vironmäki <ville.vironmaki@student.tut.fi>
2016-06-08 23:26:25 +0300

    Added user's token visible to userprofile

--- userprofile/viewbase.py
+++ userprofile/viewbase.py
@@ -4,6 +4,7 @@ from django.views.generic.base import View
 
 from lib.viewbase import BaseMixin, BaseTemplateView
 from .models import UserProfile
+from rest_framework.authtoken.models import Token
 
 
 class ACCESS(object):
@@ -23,9 +24,14 @@ class UserProfileMixin(BaseMixin):
         super().get_resource_objects()
         if self.request.user.is_authenticated():
             self.profile = UserProfile.get_by_request(self.request)
+
+            # Get user's API key
+            self.token = Token.objects.get(user=self.profile)
         else:
             self.profile = None
+        # Add available for template
         self.note("profile")
+        self.note("token")
 
     def access_control(self):
         super().access_control()
