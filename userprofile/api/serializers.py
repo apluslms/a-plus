a13068d3c4b1939d0f2cdcd26b05c14cb9003c4e
Jaakko Kantojärvi <jaakko@cs.hut.fi>
2016-06-17 14:49:22 +0300

    Split user api serializer to full and brief

--- userprofile/api/serializers.py
+++ userprofile/api/serializers.py
@@ -3,19 +3,15 @@ from course.models import CourseInstance
 from ..models import UserProfile
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    """"
-    Add the details of a user. This has to be done here because
-    details are in User-model, not in UserProfile which has OneToOneField
-    to a User-model
-    """
-    courses = serializers.SerializerMethodField('list_courses')
-    url = serializers.HyperlinkedIdentityField(view_name='api:user-detail', lookup_field='user_id', format='html')
+class UserBriefSerialiser(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        source='user.id',
+        view_name='api:user-detail',
+        lookup_url_kwarg='user_id',
+        format='html',
+    )
     user_id = serializers.IntegerField(source='user.id')
     username = serializers.CharField(source='user.username')
-    first_name = serializers.CharField(source='user.first_name')
-    last_name = serializers.CharField(source='user.last_name')
-    email = serializers.CharField(source='user.email')
 
     def list_courses(self, userinstance):
       # Get courses where the user is enrolled
@@ -34,9 +30,22 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
             'url',
             'courses',
             'user_id',
+            'username'
+        )
+
+class UserSerializer(UserBriefSerialiser):
+    """
+    Add the details of a user.
+    """
+
+    first_name = serializers.CharField(source='user.first_name')
+    last_name = serializers.CharField(source='user.last_name')
+    email = serializers.CharField(source='user.email')
+
+    class Meta(UserBriefSerialiser.Meta):
+        fields = UserBriefSerialiser.Meta.fields + (
             'student_id',
-            'username',
             'first_name',
             'last_name',
             'email',
-            )
+        )
