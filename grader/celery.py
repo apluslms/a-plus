937bcd0d5cacd3dbd482cf0eecfd6994d440ef2a
Teemu Lehtinen <teemu.t.lehtinen@aalto.fi>
2016-08-18 14:21:02 +0300

    Remove unnecessary dependencies to fix gitmanager

--- grader/celery.py
+++ grader/celery.py
@@ -4,17 +4,14 @@ Requires running Celery which requires running broker e.g. RabbitMQ.
 '''
 import os
 
+
 # Set Django configuration path for celeryd.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grader.settings')
 
+
 from celery import Celery
 from django.conf import settings
-from access.config import ConfigError
 
-
-# Check settings object and validate base dir.
-if len(settings.BASE_DIR) < 2:
-    raise ConfigError("Configuration problem, BASE_DIR: %s", settings.BASE_DIR)
 
 # Create and configure Celery instance.
 app = Celery("tasks", broker=settings.CELERY_BROKER)
