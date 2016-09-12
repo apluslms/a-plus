5ce7fb62ad31ae80f760910904f0840a0857d987
Teemu Lehtinen <teemu.t.lehtinen@aalto.fi>
2016-09-12 22:32:17 +0300

    Support courses path in importer

--- util/importer.py
+++ util/importer.py
@@ -3,8 +3,10 @@ Tools to import modules by name.
 '''
 from django.utils.module_loading import import_string
 
+from access import config
+
 
 def import_named(course, path):
     if path.startswith('.'):
-        path = 'exercises.' + course['key'] + path
+        path = config.DIR.split('/')[-1] + '.' + course['key'] + path
     return import_string(path)
