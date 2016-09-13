0c117d923f5d44a451f0c647ad98627aa6c6b0fc
Teemu Lehtinen <teemu.t.lehtinen@aalto.fi>
2016-09-13 10:52:38 +0300

    Go around circular import

--- util/importer.py
+++ util/importer.py
@@ -3,10 +3,11 @@ Tools to import modules by name.
 '''
 from django.utils.module_loading import import_string
 
-from access import config
-
 
 def import_named(course, path):
     if path.startswith('.'):
-        path = config.DIR.split('/')[-1] + '.' + course['key'] + path
+        try:
+            return import_string('exercises.' + course['key'] + path)
+        except:
+            return import_string('courses.' + course['key'] + path)
     return import_string(path)
