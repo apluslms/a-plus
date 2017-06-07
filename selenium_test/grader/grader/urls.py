71c1c1cf6a084c13fff6e6e282228bc2b3e8d319
Teemu Lehtinen <teemu.t.lehtinen@aalto.fi>
2017-06-07 08:39:01 +0300

    Upgrade selenium tests to 1.10

--- selenium_test/grader/grader/urls.py
+++ selenium_test/grader/grader/urls.py
@@ -1,14 +1,10 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 from exercises import views
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'grader.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = [
     url(r'^first_exercise/$', views.first),
     url(r'^file_exercise/$', views.file),
     url(r'^ajax_exercise/$', views.ajax, name="ajax"),
-)
+]
