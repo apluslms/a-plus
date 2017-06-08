ac4af26eeec309f7beddc7af69decc5cea0910de
Teemu Lehtinen <teemu.t.lehtinen@aalto.fi>
2017-06-08 16:36:11 +0300

    Enable testing using local docker-machine

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -9,8 +9,15 @@ EXERCISE_MOUNT=$4
 SUBMISSION_MOUNT=$5
 CMD=$6
 
+# Override host to enable local testing.
+IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -1)
+PORT=${GRADER_HOST##*:}
+GRADER_HOST=http://$IP:$PORT
+
+eval $(docker-machine env)
+
 docker run \
-  --rm \
+  -d \
   -e "SID=$SID" \
   -e "REC=$GRADER_HOST" \
   -v $EXERCISE_MOUNT:/exercise \
