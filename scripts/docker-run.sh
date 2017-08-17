28d319688f96b8e85aebb57321ed6229a4ae9a19
Teemu Lehtinen <teemu.t.lehtinen@aalto.fi>
2017-08-17 20:34:51 +0300

    Edit to support docker-compose setup

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Orders a container using docker-run command.
+# Orders a container using docker run command.
 # The container command is responsible to call /aplus/grade in end.
 
 SID=$1
@@ -9,18 +9,20 @@ EXERCISE_MOUNT=$4
 SUBMISSION_MOUNT=$5
 CMD=$6
 
-# Override host to enable local testing.
-IP=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -1)
-PORT=${GRADER_HOST##*:}
-GRADER_HOST=http://$IP:$PORT
-
-eval $(docker-machine env)
+# Manage for docker-compose setup, see test course for reference.
+TMP=/tmp/aplus
+TMP_EXERCISE_MOUNT=$TMP/_ex/${EXERCISE_MOUNT##/srv/courses/}
+TMP_SUBMISSION_MOUNT=$TMP/${SUBMISSION_MOUNT##/srv/uploads/}
+mkdir -p $(dirname $TMP_EXERCISE_MOUNT)
+mkdir -p $(dirname $TMP_SUBMISSION_MOUNT)
+cp -r $EXERCISE_MOUNT $TMP_EXERCISE_MOUNT
+cp -r $SUBMISSION_MOUNT $TMP_SUBMISSION_MOUNT
 
 docker run \
   -d \
   -e "SID=$SID" \
   -e "REC=$GRADER_HOST" \
-  -v $EXERCISE_MOUNT:/exercise \
-  -v $SUBMISSION_MOUNT:/submission \
+  -v $TMP_EXERCISE_MOUNT:/exercise \
+  -v $TMP_SUBMISSION_MOUNT:/submission \
   $DOCKER_IMAGE \
   $CMD
