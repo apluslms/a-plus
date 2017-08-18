3c7692b54caf8f16634364e7c8814f2129bc56b1
Teemu Lehtinen <teemu.t.lehtinen@aalto.fi>
2017-08-18 19:47:20 +0300

    Ensure course static is linked

---
+++ util/static.py
@@ -0,0 +1,8 @@
+import os
+from django.conf import settings
+
+def symbolic_link(courses_dir, course):
+    dst = os.path.join(settings.BASE_DIR, 'static', course['key'])
+    if not os.path.exists(dst) and 'static_dir' in course:
+        src = os.path.join(courses_dir, course['key'], course['static_dir'])
+        os.symlink(src, dst)
