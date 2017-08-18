862960de88e63166ab26026e2364be723ac9d75a
Teemu Lehtinen <teemu.t.lehtinen@aalto.fi>
2017-08-18 10:35:30 +0300

    Fix docker-compose networking

--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -10,6 +10,7 @@ SUBMISSION_MOUNT=$5
 CMD=$6
 
 # Manage for docker-compose setup, see test course for reference.
+# Docker cannot bind volume from inside docker so global /tmp is used.
 TMP=/tmp/aplus
 TMP_EXERCISE_MOUNT=$TMP/_ex/${EXERCISE_MOUNT##/srv/courses/}
 TMP_SUBMISSION_MOUNT=$TMP/${SUBMISSION_MOUNT##/srv/uploads/}
@@ -24,5 +25,6 @@ docker run \
   -e "REC=$GRADER_HOST" \
   -v $TMP_EXERCISE_MOUNT:/exercise \
   -v $TMP_SUBMISSION_MOUNT:/submission \
+  --network=aplus_default \
   $DOCKER_IMAGE \
   $CMD
