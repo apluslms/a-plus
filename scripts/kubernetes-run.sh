c33e8ec9a60f876fb8db7c8dac098987f7291f24
Markus Murhu <markus.murhu@aalto.fi>
2017-08-29 15:10:40 +0300

    Update kubernetes-run.sh with correct server and script name

--- scripts/kubernetes-run.sh
+++ scripts/kubernetes-run.sh
@@ -6,4 +6,4 @@ EXERCISE_MOUNT=$4
 SUBMISSION_MOUNT=$5
 CMD=$6
 
-ssh -o StrictHostKeyChecking=no grader@kubem "/home/grader/run-scala-grader.sh $SID $GRADER_HOST $DOCKER_IMAGE $EXERCISE_MOUNT $SUBMISSION_MOUNT"
+ssh -o StrictHostKeyChecking=no grader@k8s-admin.cs.hut.fi "/home/grader/run-grader.sh $SID $GRADER_HOST $DOCKER_IMAGE $EXERCISE_MOUNT $SUBMISSION_MOUNT"
