f0287ebc897bc318b36bcf647474c56e14a2085b
Teemu Lehtinen <teemu.t.lehtinen@aalto.fi>
2017-09-21 08:02:57 +0300

    Fix migration bug

--- course/migrations/0035_courseinstance_last_modified.py
+++ course/migrations/0035_courseinstance_last_modified.py
@@ -15,6 +15,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='courseinstance',
             name='last_modified',
-            field=models.DateTimeField(auto_now=True),
+            field=models.DateTimeField(auto_now=True, null=True),
         ),
     ]
