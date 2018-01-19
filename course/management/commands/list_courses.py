498ff98bd01e7643f3e55e4814fd9f0b36841b83
Jaakko Kantojärvi <jaakko@cs.hut.fi>
2018-01-19 02:02:32 +0200

    Add commands for course cli management

---
+++ course/management/commands/list_courses.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from course.models import CourseInstance
+
+class Command(BaseCommand):
+    help = "List all courses (course instances)"
+
+    def add_arguments(self, parser):
+        parser.add_argument('-f', '--fmt', metavar="FMT",
+                            default="{c.url}/{i.url}  {i}",
+                            help="Python format string used to print a single instance. "
+                                 "Course is in car 'c' and instance in var 'i'. "
+                                 "(e.g. '{c.id}:{i.id} {i}')")
+
+    def handle(self, *args, **options):
+        instances = CourseInstance.objects.all().order_by('course__code', 'instance_name')
+        fmt = options['fmt']
+
+        i = -1
+        for i, instance in enumerate(instances):
+            self.stdout.write(fmt.format(i=instance, c=instance.course))
+        if i < 0:
+            raise CommandError("No course instances in db")
+
---
+++ edit_course/management/commands/reload_course_configuration.py
@@ -0,0 +1,36 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from course.models import CourseInstance
+from edit_course.operations.configure import configure_content
+
+class Command(BaseCommand):
+    help = "Reload course configuration from configuration url"
+
+    def add_arguments(self, parser):
+        parser.add_argument('path', metavar="PATH", nargs='?',
+                            default="def/current",
+                            help="Path component of the course to be reconfigured from it's configuration url (default: 'def/current')")
+        parser.add_argument('-u', '--url',
+                            help="Replace current configuration url with this one")
+
+    def handle(self, *args, **options):
+        path = options['path'].strip().strip('/')
+        parts = path.split('/')
+        if len(parts) != 2:
+            raise CommandError("Path parameter neets to be in format of <course>/<instance>")
+
+        try:
+            instance = CourseInstance.objects.get(course__url=parts[0], url=parts[1])
+        except CourseInstance.DoesNotExist:
+            raise CommandError("Could not find course instance with path '{}'.".format(path))
+
+        conf_url = options['url'] or instance.configure_url
+        if not conf_url:
+            raise CommandError("There is no configuration url for {}. Use --url=<url> to set one.".format(instance))
+
+        errors = configure_content(instance, conf_url)
+        if errors:
+            self.stdout.write(self.style.ERROR("\n".join(errors)))
+            raise CommandError("Configuration failed!")
+        else:
+            self.stdout.write(self.style.SUCCESS("Course update done!"))
