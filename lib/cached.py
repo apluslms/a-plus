fe10dca57407ed2e9fda30ce7962754215d807d3
Pietari Heino <ph@extreg.com>
2018-02-13 11:27:36 +0200

    reimplement cache bug fix with cache.add()

--- lib/cached.py
+++ lib/cached.py
@@ -1,4 +1,5 @@
 from django.core.cache import cache
+from django.utils import timezone
 import logging
 
 
@@ -18,18 +19,45 @@ class CachedAbstract(object):
     @classmethod
     def invalidate(cls, *models, modifiers=[]):
         cache_key = cls._key(*models, modifiers=modifiers)
-        logger.debug("Invalidating cached data for {}".format(cache_key))
-        cache.delete(cache_key)
+        ts = timezone.now()
+        logger.debug("Invalidating cached data for {} with ts {}".format(cache_key, str(ts)))
+        invalidated = {
+                "invalidation_ts": ts
+                }
+        # overwrite the current cache with this invalidation timestamp
+        # this could be basically anything but ts is nice if debugging needs to be done
+        cache.set(cache_key, invalidated, 60*60)
 
     def __init__(self, *models, modifiers=[]):
         cache_key = self.__class__._key(*models, modifiers=modifiers)
         data = cache.get(cache_key)
+
+        # if invalidation_ts exists in the returned data, lets
+        # pass none to _needs_generation since its implementation
+        # depends on it. from _needs_generation's point of fiew
+        # nothing has changed
+        if data is not None and "invalidation_ts" in data:
+            data = None
+
         if self._needs_generation(data):
-            logger.debug("Generating cached data for {}".format(cache_key))
+
+            gen_start_ts = timezone.now()
+            logger.debug("Generating cached data for {} with ts {}".format(cache_key, str(gen_start_ts)))
             self.dirty = False
+
+            # before cache regeneration delete the entry
+            cache.delete(cache_key)
             data = self._generate_data(*models, data=data)
-            if not self.dirty:
-                cache.set(cache_key, data, None)
+
+            # if another process invalidated the cache during the time consuming
+            # generation cache.add() doesn't do anything
+            cache_updated = cache.add(cache_key, data, None)
+            if cache_updated:
+                logger.debug("Set newly generated data for {} with ts {}".format(cache_key, str(gen_start_ts)))
+            else:
+                logger.debug("Discarded generated data for {} with ts {}".format(cache_key, str(gen_start_ts)))
+
+        # in either case return the data
         self.data = data
 
     def _needs_generation(self, data):
