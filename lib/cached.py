8c8a19a5b3c2a2e1bcf7fe23702974e3bf485fb5
Jaakko Kantojärvi <jaakko@cs.hut.fi>
2018-09-20 18:58:23 +0300

    Clean lib/cached

--- lib/cached.py
+++ lib/cached.py
@@ -19,51 +19,41 @@ class CachedAbstract(object):
     @classmethod
     def invalidate(cls, *models, modifiers=[]):
         cache_key = cls._key(*models, modifiers=modifiers)
-        ts = timezone.now()
-        logger.debug("Invalidating cached data for {} with ts {}".format(cache_key, str(ts)))
-        invalidated = {
-                "invalidation_ts": ts
-                }
-        # overwrite the current cache with this invalidation timestamp
-        # this could be basically anything but ts is nice if debugging needs to be done
-        cache.set(cache_key, invalidated, 60*60)
+        logger.debug("Invalidating cached data for %s", cache_key)
+        cache.delete(cache_key)
 
     def __init__(self, *models, modifiers=[]):
         cache_key = self.__class__._key(*models, modifiers=modifiers)
         data = cache.get(cache_key)
 
-        # if invalidation_ts exists in the returned data, lets
-        # pass none to _needs_generation since its implementation
-        # depends on it. from _needs_generation's point of fiew
-        # nothing has changed
-        if data is not None and "invalidation_ts" in data:
-            data = None
-
         if self._needs_generation(data):
+            # invalidate old value
+            if data is not None:
+                cache.delete(cache_key)
 
-            gen_start_ts = timezone.now()
-            logger.debug("Generating cached data for {} with ts {}".format(cache_key, str(gen_start_ts)))
+            # generate
             self.dirty = False
-
-            # before cache regeneration delete the entry
-            cache.delete(cache_key)
+            gen_start_ts = timezone.now()
+            logger.debug("Generating cached data for %s with ts %s", cache_key, gen_start_ts)
             data = self._generate_data(*models, data=data)
 
             # if another process invalidated the cache during the time consuming
             # generation cache.add() doesn't do anything
             cache_updated = cache.add(cache_key, data, None)
             if cache_updated:
-                logger.debug("Set newly generated data for {} with ts {}".format(cache_key, str(gen_start_ts)))
+                logger.debug("Set newly generated data for %s with ts %s", cache_key, gen_start_ts)
             else:
-                logger.debug("Discarded generated data for {} with ts {}".format(cache_key, str(gen_start_ts)))
+                logger.debug("Discarded generated data for %s with ts %s", cache_key, gen_start_ts)
 
-        # in either case return the data
+                # Check that the data exists in the cache. If it doesn't then our update most likely failed
+                if cache.get(cache_key) is None:
+                    logger.error("Cache update failed for %s", cache_key)
+
+        # in either case set the data
         self.data = data
 
     def _needs_generation(self, data):
         return data is None
 
     def _generate_data(self, *models, data=None):
-        # Insert the time consuming data generation.
-        print("Failed by using CachedAbstract directly.")
-        assert False
+        raise NotImplementedError("Subclass of CachedAbstract needs to implement _generate_data")
