c1ead6659c737f30bb9e6922a8694062ad9a478c
Jaakko Kantojärvi <jaakko@cs.hut.fi>
2018-12-18 02:04:28 +0200

    Use raw id fields on diploma admin. Doesn't load all exercises from DB.

--- diploma/admin.py
+++ diploma/admin.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 from .models import CourseDiplomaDesign, StudentDiploma
 
 
-admin.site.register(CourseDiplomaDesign)
+class CourseDiplomaDesignAdmin(admin.ModelAdmin):
+    raw_id_fields = ('exercises_to_pass', 'modules_to_pass')
+
+admin.site.register(CourseDiplomaDesign, CourseDiplomaDesignAdmin)
 admin.site.register(StudentDiploma)
