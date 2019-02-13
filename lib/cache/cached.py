3a290d9b93a0617c35c8fa191c6da0f4b2b0d6d8
Jaakko Kantojärvi <jaakko.kantojarvi@aalto.fi>
2019-02-13 16:03:20 +0200

    Fix invalidation and cache update in lib.cache.cached
    Fixes issue #383
    Cache invalidation was broken by commit 8c8a19a5b3c2a2e1bcf7fe23702974e3bf485fb5
    
    Added tests to ensure this should work and notify if it's broken in the future

--- lib/cache/cached.py
+++ lib/cache/cached.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 from django.core.cache import cache
-from django.utils import timezone
+from time import time
 import logging
 
 
@@ -20,37 +21,92 @@ class CachedAbstract(object):
     def invalidate(cls, *models, modifiers=[]):
         cache_key = cls._key(*models, modifiers=modifiers)
         logger.debug("Invalidating cached data for %s", cache_key)
-        cache.delete(cache_key)
+        # The cache is invalid, if the time field is None
+        # The invalidation time is stored in the data field for debug messages
+        # Keep this value in the cache for an hour, so it will be removed from
+        # the memory at some point, but not before all generations have finished.
+        cache.set(cache_key, (None, time()), 60*60)
 
     def __init__(self, *models, modifiers=[]):
-        cache_key = self.__class__._key(*models, modifiers=modifiers)
-        data = cache.get(cache_key)
+        self.__models = models
+        self.__cache_key = self.__class__._key(*models, modifiers=modifiers)
+        self.data = self.__get_data()
 
-        if self._needs_generation(data):
-            # invalidate old value
-            if data is not None:
-                cache.delete(cache_key)
+    def __get_data(self):
+        cache_key = self.__cache_key
+        cache_name = "%s[%s]" % (self.__class__.__name__, cache_key)
 
-            # generate
-            self.dirty = False
-            gen_start_ts = timezone.now()
-            logger.debug("Generating cached data for %s with ts %s", cache_key, gen_start_ts)
-            data = self._generate_data(*models, data=data)
+        # Retrieve currently cached data
+        raw = cache.get(cache_key)
+        updated, data = raw if isinstance(raw, tuple) and len(raw) == 2 else (None, None)
 
-            # if another process invalidated the cache during the time consuming
-            # generation cache.add() doesn't do anything
-            cache_updated = cache.add(cache_key, data, None)
-            if cache_updated:
-                logger.debug("Set newly generated data for %s with ts %s", cache_key, gen_start_ts)
-            else:
-                logger.debug("Discarded generated data for %s with ts %s", cache_key, gen_start_ts)
+        # Cache is invalidated, if updated is None
+        if updated is None:
+            data = None
 
-                # Check that the data exists in the cache. If it doesn't then our update most likely failed
-                if cache.get(cache_key) is None:
-                    logger.error("Cache update failed for %s", cache_key)
+        # Use the cached data, if it doesn't require regeneration
+        # TODO: updated should be passed to _needs_generation
+        if not self._needs_generation(data):
+            return data
 
-        # in either case set the data
-        self.data = data
+        # If the cache contains invalid value, clear it
+        if raw is not None:
+            cache.delete(cache_key)
+
+        # Generate a new data
+        self.dirty = False
+        gen_start = time()
+        gen_start_dt = str(datetime.fromtimestamp(gen_start))
+        logger.debug("Generating cached data for %s with ts %s", cache_name, gen_start_dt)
+        data = self._generate_data(*self.__models, data=data)
+
+        # If another process invalidated the cache or generated a newer
+        # value for it during the generation time, then cache.add()
+        # returns False and keeps the current value in the cache
+        cache_updated = cache.add(cache_key, (gen_start, data), None)
+        if cache_updated:
+            logger.debug("Set newly generated data for %s with ts %s", cache_name, gen_start_dt)
+            # The generated value should be in the cache now
+            return data
+
+        current = cache.get(cache_key)
+        if current is None:
+            # The data was cleared before generation, but currently it is None,
+            # thus cache wasn't invalidated, so the data was probably too big.
+            # Best we can do is to log error and return the data
+            logger.error("Failed to store a value to the cache %s. It might be too big!", cache_name)
+            return data
+
+        # Someone invalidated or updated the value in the cache before we completed
+        curr_updated, curr_data = current if isinstance(current, tuple) and len(current) == 2 else (None, None)
+        if curr_updated is None:
+            # Update time is None, so data was invalidated.
+            # New value is not stored in the cache, but returned
+            try:
+                curr_dt = datetime.fromtimestamp(curr_data) if curr_data is not None else None
+            except:
+                curr_dt = repr(curr_data)
+            logger.debug("Cache %s was discarded at %s, before generation of a new data with ts %s was completed.", cache_name, curr_dt, gen_start_dt)
+        elif curr_updated > gen_start:
+            # Cache was updated before we were ready, so use the newer value
+            try:
+                curr_dt = datetime.fromtimestamp(curr_updated)
+            except:
+                curr_dt = curr_updated
+            logger.debug("Cache %s was updated at %s, before generation of a new data with ts %s was completed. Using newer value from the cache.", cache_name, curr_dt, gen_start_dt)
+            data = curr_data
+        else:
+            # We have newer value, so force the cache to this new value
+            try:
+                curr_dt = datetime.fromtimestamp(curr_updated)
+            except:
+                curr_dt = curr_updated
+            logger.debug("Cache %s was updated at %s, before generation of a new data with ts %s was completed. Updating the cache with our newer value!", cache_name, curr_dt, gen_start_dt)
+            cache.set(cache_key, (gen_start, data), None)
+            # NOTE: there is a change that the cache was invalidated between
+            # get and this set. To fix that, we would require operation
+            # check-and-set (CAS), which is not supported by Django
+        return data
 
     def _needs_generation(self, data):
         return data is None
---
+++ lib/cache/tests.py
@@ -0,0 +1,151 @@
+from django.test import SimpleTestCase
+from threading import Thread, Event, Barrier
+from unittest.mock import patch, Mock
+
+from lib.cache.cached import CachedAbstract
+
+
+class TestCached(CachedAbstract):
+    def __init__(self, func):
+        self._fake_func = func
+        super().__init__()
+
+    def _generate_data(self, *models, data=None):
+        return self._fake_func(data)
+
+
+mock_cache = {}
+
+def mock_delete(key):
+    mock_cache.pop(key, None)
+
+def mock_set(key, value, timeout=None):
+    mock_cache[key] = value
+
+def mock_get(key, default=None):
+    return mock_cache.get(key, default)
+
+def mock_add(key, value, timeout=None):
+    if key not in mock_cache:
+        mock_cache[key] = value
+        return True
+    return False
+
+
+def cache_patcher():
+    return patch.multiple('lib.cache.cached.cache',
+        add=mock_add, delete=mock_delete,
+        get=mock_get, set=mock_set)
+
+
+@cache_patcher()
+class CachedTest(SimpleTestCase):
+    def setUp(self):
+        mock_cache.clear()
+
+    def test_gen_value(self):
+        """
+        Cache should return the generated value
+        """
+        data = "Some data"
+        cached = TestCached(lambda x: data)
+        self.assertEqual(cached.data, data)
+
+    def test_gen_invalidated(self):
+        """
+        Cache should return the value generated, also after cache is invalidated
+        """
+        TestCached.invalidate()
+        data1 = "First data"
+        cached1 = TestCached(lambda x: data1)
+        self.assertEqual(cached1.data, data1)
+
+        # Repeat, to ensure invalidate works correctly too
+        TestCached.invalidate()
+        data2 = "Second data"
+        cached2 = TestCached(lambda x: data2)
+        self.assertEqual(cached2.data, data2)
+
+    def test_cache_stores_value(self):
+        """
+        Cache should store the generated value
+        """
+        data1 = "Correct data"
+        cached1 = TestCached(lambda x: data1)
+        self.assertEqual(cached1.data, data1)
+
+        cached2 = TestCached(lambda x: "Ignored data")
+        self.assertEqual(cached2.data, data1)
+
+    def test_invalidate(self):
+        """
+        If cache is invalidated during a data generation, then the data should not be cached.
+        """
+        data1 = "Wrong data"
+        def create(data):
+            TestCached.invalidate()
+            return data1
+        # thread 1 starts to create some data
+        cached1 = TestCached(create)
+        # thread 2 invalidates the cache (in create)
+        # thread 1 completes data generation
+        # thread 3 retrieves empty cache, thus generates new data
+        data3 = "Correct data"
+        cached3 = TestCached(lambda x: data3)
+        self.assertEqual(cached3.data, data3)
+
+    def test_out_of_order_update(self):
+        """
+        Cached should store the data, which generation was started at the latest point in time.
+        """
+        # thread 1 starts to create some data
+        # thread 2 starts and sets data to someting else
+        # thread 1 completes data generation
+        # thread 3 reads data from thread 2
+        data1 = "Wrong data"
+        data2 = "Correct Data"
+        def create(data):
+            cached2 = TestCached(lambda x: data2)
+            return data1
+        cached1 = TestCached(create)
+        cached3 = TestCached(lambda x: "Ignored")
+        self.assertEqual(cached3.data, data2)
+
+    def test_latest_data(self):
+        """
+        Cached should store the data, which generation was started at the latest point in time.
+        """
+        def create_generator(event, sync, data):
+            def generator(_old_data):
+                sync.wait()
+                event.wait()
+                return data
+            return generator
+        def create_thread(func):
+            def run():
+                with cache_patcher() as cache:
+                    cached = TestCached(func)
+            th = Thread(target=run)
+            th.start()
+            return th
+        # thread 1 starts to create some data
+        event1 = Event()
+        sync1 = Barrier(2, timeout=1)
+        data1 = "Wrong data"
+        t1 = create_thread(create_generator(event1, sync1, data1))
+        sync1.wait()
+        # thread 2 starts to create some data
+        event2 = Event()
+        sync2 = Barrier(2, timeout=1)
+        data2 = "Correct data"
+        t2 = create_thread(create_generator(event2, sync2, data2))
+        sync2.wait()
+        # thread 1 completes and stores data
+        event1.set()
+        t1.join()
+        # thread 2 completes and stores updated data
+        event2.set()
+        t2.join()
+        # thread 3 reads data from thread 2
+        cached3 = TestCached(lambda x: "Ignored data")
+        self.assertEqual(cached3.data, data2)
