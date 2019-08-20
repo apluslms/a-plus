927e91d9f7b5a2eab9d62e2fe4d7df14933f8da2
Henrik Milan <henrik.milan@aalto.fi>
2019-08-20 17:21:26 +0300

    Fixed exercise names to parse the selected language. Fixed jsav exercise height issues. Added splitting in exercises with novel method, by utilizing Bootstrap's grid system.

--- exammode/static/exammode/js/exam.js
+++ exammode/static/exammode/js/exam.js
@@ -1,4 +1,21 @@
-// Fix difficulty not being pulled to right on badges in panel
 $(window).on("aplus:exercise-ready", function (event) {
+    // Fix difficulty not being pulled to right on badges in panel
     $(event.target).find(".panel-title > span.pull-right").siblings("span.difficulty").addClass("pull-right");
+
+    // Split exercises instructions and form side-by-side columns with Bootstrap grid system
+    let exercise = $(event.target).find("#exercise")
+
+    if (exercise.length && exercise.children("form").length == 1 && exercise.children("div").length == 1) {
+        exercise.addClass("row");
+        exercise.children("form").addClass("col-xs-6");
+        exercise.children("div").addClass("col-xs-6");
+    }
 });
+
+$(window).on('shown.bs.collapse', function(event) {
+    // Reload iframe after opening panel to reset height
+    let iframe = $(event.target).find("iframe");
+    if (iframe.length) {
+        iframe[0].src = iframe[0].src
+    }
+})
