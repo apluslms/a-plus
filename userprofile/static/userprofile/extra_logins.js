e6391901b124efade2ec8ef01fdf43042f66c732
Ella Anttila <ella.anttila@aalto.fi>
2020-06-22 11:03:41 +0300

    Change UI of the local user login form
    * Switch to use django form for the local user login.
    It adds labels to login form to improve accessibility and also
    provides error handling. Update extra_logins.js so that if logins
    have error messages, extra logins are not hidden.
    
    * Change the color of the button so it looks more like a button.
    
    Fixes https://github.com/apluslms/a-plus/issues/582
    
    Switch local user login to use django form

--- userprofile/static/userprofile/extra_logins.js
+++ userprofile/static/userprofile/extra_logins.js
@@ -2,16 +2,18 @@
 
   function showExtras(event) {
     event.preventDefault();
-    const extras = $("#login-box-row").find(".extra-login")
+    const extras = $("#login-box-row").find(".extra-login");
     extras.show();
     $(".show-extra-login-btn").hide();
-    extras.find(":input:visible").first().focus() // focus for keyboard navigation
+    extras.find(":input:visible").first().focus(); // focus for keyboard navigation
   }
 
   $(function () {
-    const row = $("#login-box-row")
-    const normal = row.find(".login-box").not(".extra-login")
-    if (normal.length > 0) {
+    const row = $("#login-box-row");
+    const normal = row.find(".login-box").not(".extra-login");
+    const errors = row.find(".extra-login").find(".alert");
+    // if any of the extra logins have errors, none of them are hidden
+    if (normal.length > 0 && errors.length == 0) {
       row.find(".extra-login").hide();
       $(".show-extra-login-btn").on("click", showExtras).show();
 
