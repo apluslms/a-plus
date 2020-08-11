ba372f1c0a4cb6a8dd1181d52ea8b6316afde4f7
Sundell Henrik <henrik.sundell@gmail.com>
2020-08-11 13:52:41 +0300

    Add custom SessionAuth for api

---
+++ lib/api/authentication/api_authentication.py
@@ -0,0 +1,44 @@
+from django.middleware.csrf import CsrfViewMiddleware
+
+class CSRFCheck(CsrfViewMiddleware):
+    def _reject(self, request, reason):
+        # Return the failure reason instead of an HttpResponse
+        return reason
+
+class SessionAuthentication:
+    """
+    Use Django's session framework for authentication.
+    """
+
+    def authenticate(self, request):
+        """
+        Returns a `User` if the request session currently has a logged in user.
+        Otherwise returns `None`.
+        """
+        # Get the session-based user from the underlying HttpRequest object
+        user = getattr(request._request, 'user', None)
+
+        # Unauthenticated, CSRF validation not required
+        if not user or not user.is_active:
+            return None
+        
+        self.enforce_csrf(request)
+        
+        if 'Authorization' in request.headers:
+            print(request.headers['Authorization'].split()[1])
+            token = request.headers['Authorization'].split()[1]
+        
+        # CSRF passed with authenticated user
+        return (user, None)
+
+    def enforce_csrf(self, request):
+        """
+        Enforce CSRF validation for session based authentication.
+        """
+        check = CSRFCheck()
+        # populates request.META['CSRF_COOKIE'], which is used in process_view()
+        check.process_request(request)
+        reason = check.process_view(request, None, (), {})
+        if reason:
+            # CSRF failed, bail with explicit error message
+            raise exceptions.PermissionDenied('CSRF Failed: %s' % reason)
