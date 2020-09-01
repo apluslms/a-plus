e1d00dab367959f70186085d4815179b6dde488e
Jaakko Kantojärvi <jaakko.kantojarvi@aalto.fi>
2020-09-01 16:07:26 +0300

    Add function to get list of all localized variants of a string

--- lib/localization_syntax.py
+++ lib/localization_syntax.py
@@ -29,3 +29,27 @@ def pick_localized(entry, lang):
             if ':' in variant:
                 return prefix + variant.split(':', 1)[1] + suffix
     return text
+
+
+def parse_localized(entry):
+    """
+    Returns a list of pairs of language and value from
+    |lang:value|lang:value| -format text.
+    If there is no language, then the first item in the tuple will be None.
+    """
+    text = entry if isinstance(entry, str) else str(entry)
+    variants = text.split('|')
+    if len(variants) < 2:
+        return [(None, text)]
+
+    prefix = variants[0]
+    suffix = variants[-1]
+    entries = []
+    for variant in variants[1:-1]:
+        if not variant:
+            continue
+        lang, colon, data = variant.partition(':')
+        if not colon or data.startswith('//') or len(lang.split('-')[0]) > 3:
+            lang, data = None, variant
+        entries.append((lang, ''.join((prefix, data, suffix))))
+    return entries
