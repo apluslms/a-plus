6a3d1e5342f234fc85f915e66a31b099093cc609
Markku Riekkinen <markku.riekkinen@aalto.fi>
2020-11-20 14:27:20 +0200

    Add command for counting active users based on last login

---
+++ userprofile/management/commands/count_active_users.py
@@ -0,0 +1,23 @@
+import datetime
+
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+
+class Command(BaseCommand):
+    help = 'Count active users based on the last_login timestamp'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-d', '--days-ago', type=int, default=365,
+            help='Count the number of users who have logged in in the past N days. '
+            'By default, the last 365 days are used. Provide the number of days as a whole number.'
+        )
+
+    def handle(self, *args, **options):
+        since_delta = datetime.timedelta(days=options['days_ago'])
+        since = timezone.now() - since_delta
+        num_users = User.objects.filter(last_login__gte=since).count()
+        self.stdout.write("{0} users have logged in since {1}".format(num_users, since))
+
