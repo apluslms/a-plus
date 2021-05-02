ff94c4b336129738c9cce6d9d65cd659bb8b3841
Pasi Sarolahti <pasi.sarolahti@iki.fi>
2021-05-02 19:32:28 +0300

    Add separate button for Aalto login
    Add back the Aalto login button that was replaced by Haka login
    button in recent commit. Now both local Aalto login button
    and Haka login button for external users is shown.
    Closes #815.

--- shibboleth_login/urls.py
+++ shibboleth_login/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^login/$', views.login, name="shibboleth-login"),
+    url(r'^shibboleth/login/$', views.login, name="shibboleth-login"),
+    url(r'^Shibboleth.sso/haka_login$', views.login, name="haka-login"),
 ]
 
 if settings.DEBUG:
