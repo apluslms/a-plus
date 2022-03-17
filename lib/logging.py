0c60dc2e2f52665f305199d6cda6b6dd2e4bf737
Pasi Sarolahti <pasi.sarolahti@iki.fi>
2022-03-17 16:16:00 +0200

    Apply CEF format in security logs
    Aalto ITS requires ArcSight Common Event Format (CEF) for the
    stored logs. Convert the SecurityLog output to follow this format,
    using format_cef library.
    
    Fixes #971.

--- lib/logging.py
+++ lib/logging.py
@@ -3,6 +3,12 @@ import logging
 from django.contrib.auth.signals import user_logged_in, user_logged_out, user_login_failed
 from django.dispatch import receiver
 from django.http.request import HttpRequest
+from format_cef import format_cef
+from aplus import VERSION
+
+
+CEF_VENDOR = 'Aalto'
+CEF_PRODUCT = 'aplus'
 
 def skip_unreadable_post(record):
     """Skips log records of unfinished post requests."""
@@ -10,7 +16,8 @@ def skip_unreadable_post(record):
 
 class SecurityLog:
     """
-    Static singleton class used for A+ - wide security logging.
+    Static singleton class used for A+ - wide security logging,
+    to produce ArcSight Common Event Format (CEF) log.
     Django signals are used to track login/logout events.
     """
 
@@ -21,20 +28,32 @@ class SecurityLog:
         return SecurityLog.seclogger
 
     @staticmethod
-    def logevent(request: HttpRequest, type: str, message: str) -> None:
-        # Unit tests do not have user defined in request object.
-        if request and hasattr(request, 'user'):
-            user=request.user
-            ip=request.META.get('REMOTE_ADDR')
-        else:
-            user='?'
-            ip='?'
-        SecurityLog.logger().info("({}/{}): {}: {}".format(
-            user,
-            ip,
-            type,
-            message
-        ))
+    def logevent(
+            request: HttpRequest,
+            type: str,
+            message: str,
+            severity: int = 5,
+            ) -> None:
+        extensions = {}
+        # Unit tests may not have user or source address defined.
+        if request:
+            if hasattr(request, 'user'):
+                extensions['sourceUserName'] = str(request.user)
+                extensions['sourceUserId'] = str(request.user.id)
+            if (addr := request.META.get('REMOTE_ADDR')):
+                extensions['sourceAddress'] = addr
+
+        SecurityLog.logger().info(
+            format_cef(
+                CEF_VENDOR,
+                CEF_PRODUCT,
+                VERSION,
+                type,
+                message,
+                severity,
+                extensions,
+            ).decode("utf-8")
+        )
 
 
 # This example was used as an inspiration for the following functions:
@@ -51,6 +70,8 @@ def user_logged_out_callback(sender, request, user, **kwargs):
 # so we cannot get the IP.
 @receiver(user_login_failed)
 def user_login_failed_callback(sender, credentials, **kwargs):
-    SecurityLog.logevent(None, "login-failed","{credentials}".format(
-        credentials=credentials)
-    )
+    try:
+        SecurityLog.logevent(None, "login-failed", f"username: {credentials['username']}")
+    except KeyError:
+        # Unit tests do not have 'username' in credentials, let's not fail them for that
+        pass
