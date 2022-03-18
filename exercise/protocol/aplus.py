apluslms/a-plus

commit 559fa6b6aebdc2159645cb66f9e433303fcf4c03

Remove duplicate HTML attribute id="exercise"
Fixes #940

lib/remote_page.py
@@ -175,6 +175,8 @@ class RemotePage:
 
     def element_or_body(self, search_attributes):
         element = self.select_element_or_body(search_attributes)
+        if element.get('id') == 'exercise':
+            del element['id']
         return str(element) if element else ""
 
     def clean_element_or_body(self, search_attributes):

commit f3242575d76b63eca7e646f084a436b958b6a51d

Fix bug: select the correct element from grader feedback HTML
Commit 559fa6b6aebdc2159645cb66f9e433303fcf4c03 introduced new bugs.
It removed the id="exercise" attribute from the html received from
the exercise service without taking the method
`clean_element_or_body` into account.

The method `clean_element_or_body` was called after
`element_or_body`, thus when `element_or_body` removed
the id attribute, finding the same id attribute failed in
`clean_element_or_body`. Therefore, `clean_element_or_body` returned
the whole body element instead of the wanted div element.
The output of `clean_element_or_body` is particularly used in
the submission feedback, thus the feedback layout got broken when
it included unwanted elements from the whole body received from
the grader.

This commit combines the methods `element_or_body` and
`clean_element_or_body` so that the target div can be found before
it is removed from the html content.

exercise/protocol/aplus.py
@@ -1,4 +1,6 @@
 import logging
+from typing import TYPE_CHECKING
+
 from django.contrib import messages
 from django.utils.text import format_lazy
 from django.utils.translation import gettext_lazy as _
@@ -6,6 +8,10 @@ from django.utils.translation import gettext_lazy as _
 from lib.email_messages import email_course_error
 from lib.remote_page import RemotePage, RemotePageException
 from .exercise_page import ExercisePage
+
+
+if TYPE_CHECKING:
+    from exercise.models import LearningObject
 
 
 logger = logging.getLogger("aplus.protocol")
@@ -109,7 +115,11 @@ def load_feedback_page(request, url, exercise, submission, no_penalties=False):
     return page
 
 
-def parse_page_content(page, remote_page, exercise):
+def parse_page_content(
+        page: ExercisePage,
+        remote_page: RemotePage,
+        exercise: 'LearningObject',
+        ) -> None:
     """
     Parses exercise page elements.
     """
@@ -160,7 +170,7 @@ def parse_page_content(page, remote_page, exercise):
         {'id':'chapter'},
         {'class':'entry-content'},
     )
-    page.content = remote_page.element_or_body(element_selectors)
-    page.clean_content = remote_page.clean_element_or_body(element_selectors)
+    id_attrs_to_remove = ('exercise', 'chapter', 'aplus')
+    page.content, page.clean_content = remote_page.element_or_body(element_selectors, id_attrs_to_remove)
     page.last_modified = remote_page.last_modified()
     page.expires = remote_page.expires()

lib/remote_page.py
@@ -2,10 +2,10 @@ import logging
 import posixpath
 import re
 import time
-from typing import Optional
+from typing import Mapping, Optional, Sequence, Tuple
 from urllib.parse import urlparse, urljoin
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 import requests
 from requests.models import Response
 
@@ -164,7 +164,10 @@ class RemotePage:
                 self.soup.head.find_all(True, search_attribute))
         return ""
 
-    def select_element_or_body(self, search_attributes):
+    def select_element_or_body(
+            self,
+            search_attributes: Sequence[Mapping[str, str]],
+            ) -> Optional[Tag]:
         if self.soup:
             for attr in search_attributes:
                 element = self.soup.find(**attr)
@@ -173,21 +176,39 @@ class RemotePage:
             return self.soup.body
         return None
 
-    def element_or_body(self, search_attributes):
-        element = self.select_element_or_body(search_attributes)
-        if element.get('id') == 'exercise':
-            del element['id']
-        return str(element) if element else ""
-
-    def clean_element_or_body(self, search_attributes):
+    def element_or_body(
+            self,
+            search_attributes: Sequence[Mapping[str, str]],
+            id_attrs_to_remove: Optional[Sequence[str]] = None,
+            ) -> Tuple[str, str]:
+        result = clean_result = ''
         element = self.select_element_or_body(search_attributes)
         if element:
-            for once in element.find_all(True, {'data-aplus-once':True}):
-                once.extract()
-        return str(element) if element else ""
+            self._remove_id_attr(element, id_attrs_to_remove)
+            result = str(element)
+            clean_result = self._clean_element(element)
+        return result, clean_result
 
-    def body(self):
-        return self.element_or_body([])
+    def _clean_element(self, element: Tag) -> str:
+        for once in element.find_all(True, {'data-aplus-once':True}):
+            once.decompose()
+        return str(element)
+
+    def _remove_id_attr(
+            self,
+            element: Tag,
+            id_values_to_remove: Optional[Sequence[str]],
+            ) -> None:
+        """Remove the id attribute from the element
+        if the id value is one of the values given in id_values_to_remove.
+        """
+        if not id_values_to_remove:
+            return
+        if element.get('id') in id_values_to_remove:
+            del element['id']
+
+    def body(self) -> Optional[Tag]:
+        return self.soup.body if self.soup else None
 
     def fix_relative_urls(self):
         url = self.base_address()