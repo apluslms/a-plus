25733db9a2bcf53e1c968f12f5e23b744824f291
Pasi Sarolahti <pasi.sarolahti@iki.fi>
2022-08-11 08:41:54 +0300

    Start SISU enrolling 14 days prior to course start
    Previously automatic SIS enrolling started only at the course
    start time, but some teachers would like to see the participants
    already before the start of the course.

--- aplus/celery.py
+++ aplus/celery.py
@@ -1,6 +1,7 @@
 import os
 import celery
 import datetime
+from datetime import timedelta
 from time import sleep
 
 from django.conf import settings
@@ -23,9 +24,10 @@ def enroll():
     """
     from course.models import CourseInstance
     now = datetime.datetime.now(datetime.timezone.utc)
+    # Enroll students for active courses, or those that will start in 14 days
     courses = CourseInstance.objects.filter(
         ending_time__gt=now,
-        starting_time__lt=now,
+        starting_time__lt=now + timedelta(days=14),
         sis_enroll=True,
     )
     for i in courses:
