dc6d100e3392c17533f9e5e80c8db1705f9b54a7
Markku Riekkinen <markku.riekkinen@aalto.fi>
2022-11-15 12:40:44 +0200

    Fix crashes in the API filter FieldValuesFilter
    Users could crash the server by sending invalid values in
    the URL GET query parameters `field` and `values`.
    
    The API filter backend `FieldValuesFilter` is used in the user and
    student list API endpoints.

--- lib/api/filters.py
+++ lib/api/filters.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.query import QuerySet
+from django.http import Http404
 
 from rest_framework import filters
 from rest_framework.request import Request
@@ -24,8 +25,13 @@ class FieldValuesFilter(filters.BaseFilterBackend):
         param_field = request.query_params.get('field', '')
         if not param_field:
             return queryset
-        field = view.field_values_map[param_field]
+        field = view.field_values_map.get(param_field)
+        if not field:
+            raise Http404("Invalid 'field' query parameter")
         param_values = request.query_params.get('values', '')
         values = [value.strip() for value in param_values.split(',')]
         kwargs = {f'{field}__in': values}
-        return queryset.filter(**kwargs)
+        try:
+            return queryset.filter(**kwargs)
+        except ValueError as exc:
+            raise Http404(f"Invalid filters in FieldValuesFilter: {kwargs}") from exc
