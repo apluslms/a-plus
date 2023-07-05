c5c92a815396a3bb8ebb51a2c7092c2362a326db
Tuomas Laine <tuomaslaine97@gmail.com>
2023-07-05 08:16:07 +0000

    Store each cache object in inheritance separately
    Each of an object's ancestors are stored separately. Saves space in the
    cache as the cached points versions can use the data from the non-points
    versions.

--- lib/cache/cached.py
+++ lib/cache/cached.py
@@ -3,6 +3,7 @@ from copy import deepcopy
 from dataclasses import InitVar
 from datetime import datetime
 import itertools
+import pickle
 from time import time
 from typing import (
     Any,
@@ -13,6 +14,7 @@ from typing import (
     Iterable,
     List,
     Optional,
+    Set,
     Tuple,
     Type,
     TYPE_CHECKING,
@@ -130,18 +132,16 @@ def resolve_proxies(
 
     proxies = [proxy for proxy in proxies if not proxy._resolved]
 
-    keys = {proxy._cache_key for proxy in proxies}
+    keys = {k for proxy in proxies for k in proxy._keys}
     items = {k: v[1] for k,v in cache.get_many(keys).items()}
 
-    new_states = {}
+    nstates: Dict[str, Tuple[float, bytes]] = {}
     for proxy in proxies:
-        nstate = proxy._build(items, precreated, prefetched_data)
-        if nstate:
-            new_states[proxy._cache_key] = (proxy._generated_on, nstate)
+        proxy._build(items, nstates, precreated, prefetched_data)
 
+    stored_states = cache.get_many(nstates.keys())
     save_states = {}
-    stored_states = cache.get_many(new_states.keys())
-    for k, nstate in new_states.items():
+    for k, nstate in nstates.items():
         stored_state = stored_states.get(k)
         if (
             stored_state is None
@@ -171,27 +171,34 @@ def _dc(obj, memo):
 
 
 if TYPE_CHECKING:
-    # This makes NoCache[T] look like T to type checkers
+    # This makes NoCache[T] and Varies[T] look like T to type checkers
     NoCache = ClassVar
+    Varies = ClassVar
 else:
     # NoCache[T] is swapped with T in annotations by CacheMeta
     from typing import Generic
-    NotCached = TypeVar("NotCached")
-    class NoCache(Generic[NotCached]): ...
+    CacheT = TypeVar("CacheT")
+    class NoCache(Generic[CacheT]):
+        """Do not add the field to the cache"""
+    class Varies(Generic[CacheT]):
+        """Add the field to the cache separately for each object in the inheritance tree"""
 
 
-class CacheMeta(type):
-    """A metaclass for CacheBase to set the _cached_fields attribute automatically for each subclass"""
-    _cached_fields: Tuple[str, ...]
+def get_cache_fields(cls) -> Tuple[Tuple[str, ...], Tuple[str, ...]]:
+    if not hasattr(cls, "__dict__"):
+        return (), ()
 
-    def __new__(cls, name, bases, namespace, **kwargs):
-        ncls = super().__new__(cls, name, bases, namespace, **kwargs)
+    if "_cached_fields" in cls.__dict__:
+        return cls.__dict__["_cached_fields"], cls.__dict__["_varying_fields"]
 
-        globals = sys.modules[ncls.__module__].__dict__.copy()
-        globals[ncls.__name__] = ncls
+    nocache_fields = set()
+    cached_fields = set()
+    varying_fields = set()
 
-        cached_fields = set()
-        annotations = ncls.__dict__.get('__annotations__', {})
+    annotations = cls.__dict__.get('__annotations__', {})
+    if annotations:
+        m_globals = sys.modules[cls.__module__].__dict__.copy()
+        m_globals[cls.__name__] = cls
         for k, ty in annotations.items():
             # get_type_hints returns base class annotations as well but we dont want those.
             # That also causes issues when the baseclasses use types that aren't imported
@@ -200,30 +207,99 @@ class CacheMeta(type):
             if ty is None:
                 ty = type(None)
             if isinstance(ty, str):
-                ty = eval(ty, globals, namespace)
+                # pylint: disable-next=eval-used
+                ty = eval(ty, m_globals, cls.__dict__)
 
             origin = get_origin(ty)
             if origin is NoCache:
+                nocache_fields.add(k)
                 # Remove NoCache from annotations
-                ncls.__annotations__[k] = get_args(ty)[0]
+                cls.__annotations__[k] = get_args(ty)[0]
             elif origin is not ClassVar and not isinstance(ty, InitVar):
                 # Add non-NoCache and non-ClassVar to cached fields list
                 cached_fields.add(k)
+                if origin is Varies:
+                    varying_fields.add(k)
 
-        sorted_fields = sorted(cached_fields.union(*(getattr(base, "_cached_fields", []) for base in bases)))
-        # Would use a set but we need the order of the keys to always be the same for caching to work correctly
-        ncls._cached_fields = tuple(sorted_fields)
+    # Add the cache fields from parents that aren't loaded separately
+    parents = cls.__dict__.get("_parents", ())
+    for base in cls.__bases__:
+        cached, varying = get_cache_fields(base)
+        varying_fields.update(varying)
+
+        if base in parents:
+            continue
+
+        cached_fields.update(cached)
+
+    cached_fields -= nocache_fields
+    varying_fields -= nocache_fields
+    cached_fields.update(varying_fields)
+
+    # Would use a set but we need the order of the keys in cached_fields to always be the same for caching
+    # to work correctly
+    return tuple(sorted(cached_fields)), tuple(varying_fields)
+
+
+class CacheMeta(type):
+    """A metaclass for CacheBase to set the _cached_fields attribute automatically for each subclass"""
+    # Use PARENTS: Tuple[CacheMeta, ...] to manually determine the parent classes
+    KEY_PREFIX: str
+    NUM_PARAMS: int
+    _cached_fields: Tuple[str, ...]
+    _varying_fields: Tuple[str, ...]
+    _all_cached_fields: Set[str]
+    _parents: Tuple[Type[CacheBase], ...]
+
+    def __new__(cls, name, bases, namespace, **kwargs):
+        ncls = super().__new__(cls, name, bases, namespace, **kwargs)
+
+        if "KEY_PREFIX" in ncls.__dict__ and "NUM_PARAMS" not in ncls.__dict__:
+            raise ValueError(
+                f"Instances (classes) of CacheMeta must have both KEY_PREFIX"
+                " and NUM_PARAMS or neither. {name} is missing NUM_PARAMS."
+            )
+        elif "KEY_PREFIX" not in ncls.__dict__ and "NUM_PARAMS" in ncls.__dict__:
+            raise ValueError(
+                f"Instances (classes) of CacheMeta must have both KEY_PREFIX"
+                " and NUM_PARAMS or neither. {name} is missing KEY_PREFIX"
+            )
+
+        cache_meta_mro: List[CacheMeta] = [base for base in ncls.__mro__ if isinstance(base, CacheMeta)]
+        cached_mro: List[CacheMeta] = [base for base in cache_meta_mro if "KEY_PREFIX" in base.__dict__]
+
+        if "PARENTS" in ncls.__dict__:
+            parents = ncls.__dict__["PARENTS"]
+            for base in parents:
+                if not isinstance(base, CacheMeta) or "KEY_PREFIX" not in base.__dict__:
+                    raise ValueError(f"{base} is not a valid cache parent of {ncls}")
+            ncls._parents = tuple([*parents, ncls])
+        else:
+            skip = set()
+            for base in ncls.__bases__:
+                if "_parents" in base.__dict__:
+                    base_parents = base.__dict__["_parents"]
+                    skip.update(base2 for base2 in base.__bases__ if base2 not in base_parents)
+
+            parents = []
+            for base in reversed(cached_mro):
+                if base not in skip:
+                    parents.append(base)
+            ncls._parents = tuple(parents)
+
+        ncls._cached_fields, ncls._varying_fields = get_cache_fields(ncls)
+        ncls._all_cached_fields = {f for c in ncls._parents for f in c._cached_fields}
 
         return ncls
 
 
 T = TypeVar("T", bound="CacheBase")
 class CacheBase(metaclass=CacheMeta):
-    KEY_PREFIX = 'cache_base'
-    _cache_key: NoCache[str]
+    _keys_with_cls: NoCache[List[Tuple[Type[CacheBase], str]]]
+    _keys: NoCache[List[str]]
     _resolved: NoCache[bool]
-    _params: Tuple[Any, ...]
-    _generated_on: float
+    _params: NoCache[Tuple[Any, ...]]
+    _generated_on: Varies[float]
 
     def __deepcopy__(self, memo):
         obj = self.__class__.__new__(self.__class__)
@@ -238,12 +314,21 @@ class CacheBase(metaclass=CacheMeta):
     def post_get(self, precreated: PrecreatedProxies):
         """
         Called after the object was loaded from cache. Child proxies of self
-        are just the _params tuple instead of the actual objects. This method
-        must replace the child proxy tuples with actual objects. Get the object
-        from precreated or create a new proxy if not found inside it (see the
-        get_or_create_proxy function).
+        are just the _params tuple instead of the actual objects, unless a
+        parent class' _generate_data or post_get has changed them.
+
+        This is just a useful hook, and does not have to be implemented.
         """
-        raise NotImplementedError()
+
+    def post_build(self, precreated: PrecreatedProxies):
+        """
+        Called after the whole object has been built (each object in the inheritance
+        tree has been loaded from the cache or generated). This method
+        must replace the child proxy tuples with actual objects (if not done
+        previously by _generate_data or post_get). Get the object from precreated
+        or create a new proxy if not found inside it (see the get_or_create_proxy
+        function).
+        """
 
     def is_valid(self) -> bool:
         return True
@@ -307,7 +392,7 @@ class CacheBase(metaclass=CacheMeta):
             objdict[name] = value
 
     def __getattr__(self, name: str) -> Any:
-        if not self._resolved and name in self.__class__._cached_fields:
+        if not self._resolved and name in self.__class__._all_cached_fields:
             logger.debug(f"Lazy resolving {self!r} due to missing {name}")
             resolve_proxies([self])
             return getattr(self, name)
@@ -327,7 +412,8 @@ class CacheBase(metaclass=CacheMeta):
         obj = cls.__new__(cls)
         obj._resolved = False
         obj._params = params
-        obj._cache_key = cls._key(*params)
+        obj._keys_with_cls = cls._get_keys_with_cls(*params)
+        obj._keys = [a for _, a in obj._keys_with_cls]
         return obj
 
     @classmethod
@@ -337,24 +423,24 @@ class CacheBase(metaclass=CacheMeta):
 
     @classmethod
     def get(cls: Type[T], *params, prefetch_children: bool = False, prefetched_data: Optional[DBData] = None) -> T:
-        cache_key = cls._key(*params)
-
         obj = cls.proxy(*params)
 
-        data = cache.get(cache_key)
-        if data is None or not isinstance(data, tuple):
-            data = {}
-        else:
-            data = {cache_key: data[1]}
+        data = {k: v[1] for k,v in cache.get_many(obj._keys).items()}
 
-        nstate = obj._build(data, {}, prefetched_data)
-        if nstate:
-            stored_obj = cache.get(cache_key)
-            if (
-                stored_obj is None
-                or stored_obj[0] <= obj._generated_on
-            ):
-                cache.set(cache_key, (obj._generated_on, nstate))
+        nstates: Dict[str, Tuple[float, bytes]] = {}
+        obj._build(data, nstates, {}, prefetched_data)
+        if nstates:
+            stored_states = cache.get_many(nstates.keys())
+            save_states = {}
+            for k, nstate in nstates.items():
+                stored_state = stored_states.get(k)
+                if (
+                    stored_state is None
+                    or stored_state[0] <= nstate[0]
+                ):
+                    save_states[k] = nstate
+
+            cache.set_many(save_states)
 
         if prefetch_children:
             obj.populate_children(prefetched_data)
@@ -362,13 +448,17 @@ class CacheBase(metaclass=CacheMeta):
         return obj
 
     @classmethod
-    def _key(cls, *params) -> str:
-        id_str = ','.join(str(p) for p in params)
-        return f"{cls.KEY_PREFIX}:{id_str}"
+    def _get_keys_with_cls(cls, *params) -> List[Tuple[Type[CacheBase], str]]:
+        keys = []
+        for p in cls._parents:
+            id_str = ','.join(str(p) for p in params[:p.NUM_PARAMS])
+            cache_key = f"{p.KEY_PREFIX}:{id_str}"
+            keys.append((p, cache_key))
+        return keys
 
     @classmethod
     def invalidate(cls, *models):
-        cache_key = cls._key(*cls.parameter_ids(*models))
+        _, cache_key = cls._get_keys_with_cls(*cls.parameter_ids(*models))[-1]
         logger.debug(f"Invalidating cached data for {cls.__name__}[{cache_key}]")
         # The cache is invalid, if the time field is None
         # The invalidation time is stored in the data field for debug messages
@@ -379,24 +469,41 @@ class CacheBase(metaclass=CacheMeta):
     def _build(
             self,
             cache_data: Dict[str, Any],
+            new_cache_data: Dict[str, Tuple[float, bytes]],
             precreated: PrecreatedProxies,
             prefetched_data: Optional[DBData],
-            ) -> Optional[Tuple[Any, ...]]:
-        attrs = cache_data.get(self._cache_key)
-        if attrs is not None:
-            try:
-                self.__setstate__(attrs)
-            except TypeError as e:
-                logger.warning(f"__setstate__ TypeError with {self.__class__.__name__}[{self._cache_key}]: {e}")
-                attrs = None # Generate new cache data
-            else:
-                self.post_get(precreated)
+            ):
+        ocls = self.__class__
+        base_cls = None
 
-        if attrs is None or not self.is_valid():
-            self._get_data(precreated, prefetched_data)
-            return self.__getstate__()
+        # Set _resolved to true to make sure that _get_data doesn't accidentally trigger lazy resolving
+        # or recursively try to resolve self again
+        self._resolved = True
+        for base_cls, cache_key in self._keys_with_cls:
+            # Trick python to use the base_cls' versions of methods instead of the original class'.
+            # This should ensure that _get_data (and _generate_data) work correctly even if self
+            # is actually an instance of a different class.
+            self.__class__ = base_cls
 
-        return None
+            attrs = cache_data.get(cache_key)
+            if attrs is not None:
+                try:
+                    self.__setstate__(pickle.loads(attrs))
+                except TypeError as e:
+                    logger.warning(f"__setstate__ TypeError with {base_cls}[{cache_key}]: {e}")
+                    attrs = None # Generate new cache data
+                else:
+                    self.post_get(precreated)
+
+            if attrs is None or not self.is_valid():
+                self._get_data(precreated, prefetched_data)
+                new_cache_data[cache_key] = (self._generated_on, pickle.dumps(self.__getstate__()))
+
+        # Do not reset the class back if _get_data/unpickler changed the type
+        if self.__class__ is base_cls:
+            self.__class__ = ocls
+
+        self.post_build(precreated)
 
     def _get_data(
             self,
@@ -408,11 +515,10 @@ class CacheBase(metaclass=CacheMeta):
         else:
             gen_start = prefetched_data.gen_start
         gen_start_dt = str(datetime.fromtimestamp(gen_start))
-        cache_name = "%s[%s]" % (self.__class__.__name__, self._cache_key)
+        cache_name = "%s[%s]" % (self.__class__.__name__, self._params)
         logger.debug("Generating cached data for %s with ts %s", cache_name, gen_start_dt)
         self._generate_data(precreated=precreated, prefetched_data=prefetched_data)
         self._generated_on = gen_start
-        self._resolved = True
 
     @classmethod
     def parameter_ids(cls, *models: Any) -> Tuple[Any, ...]:
