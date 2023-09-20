apluslms/a-plus

commit 0e74906b156b9f3da07ac198760d1133e5038b2a

Fix non-revealed chapters in ModuleView
Fixes #1266

course/viewbase.py
@@ -4,6 +4,7 @@ from django.utils.translation import get_language, get_language_info
 
 from authorization.permissions import ACCESS
 from exercise.cache.content import CachedContent
+from exercise.cache.points import CachedPoints
 from lib.helpers import remove_query_param_from_url, update_url_params
 from lib.viewbase import BaseTemplateView
 from userprofile.viewbase import UserProfileMixin
@@ -67,11 +68,12 @@ class CourseInstanceBaseMixin:
             self.url_without_language = remove_query_param_from_url(self.request.get_full_path(), 'hl')
             self.query_language = None
             self.user_language = None
+            self.points = CachedPoints(self.instance, self.request.user, self.content, self.is_course_staff)
 
             self.note(
                 "course", "instance", "content", "user_course_data", "is_student", "is_assistant",
                 "is_teacher", "is_course_staff", "get_taggings", "url_without_language",
-                "query_language", "user_language"
+                "query_language", "user_language", "points"
             )
 
             # Try to find a language that is defined for this course instance

course/views.py
@@ -241,8 +241,8 @@ class ModuleView(CourseModuleBaseView):
         super().get_common_objects()
         self.now = timezone.now()
         try:
-            self.children = self.content.flat_module(self.module)
-            cur, _tree, prev, nex = self.content.find(self.module)
+            self.children = self.points.flat_module(self.module)
+            cur, _tree, prev, nex = self.points.find(self.module)
             self.previous = prev
             self.current = cur
             self.next = nex

commit a8e76f7e7463c19be1eb31c04781f07f9de90682

Fix crash in CachedPoints with GraderUser from commit 0e74906b
Commit 0e74906b156b9f3da07ac198760d1133e5038b2a (#1273) introduced
a crash when `GraderUser` accesses any course page or API.
The commit added a `CachedPoints` instance variable to
`CourseInstanceBaseMixin`, but `CachedPoints` crashes when it is
initialized with the `GraderUser`. `CachedPoints` does not crash
with `AnonymousUser`, thus it is used instead of `GraderUser`
to work around the crash.

In the master branch, the commit
0c0de05a2e2fa9b14d2247088c5ad87bc9592884 is similar to 0e74906b,
but there is no crash due to the cache changes in #1231.

course/viewbase.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AnonymousUser
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import translation
 from django.utils.translation import get_language, get_language_info
@@ -7,6 +8,7 @@ from exercise.cache.content import CachedContent
 from exercise.cache.points import CachedPoints
 from lib.helpers import remove_query_param_from_url, update_url_params
 from lib.viewbase import BaseTemplateView
+from userprofile.models import GraderUser
 from userprofile.viewbase import UserProfileMixin
 from exercise.models import LearningObject
 from .cache.students import CachedStudent
@@ -68,7 +70,12 @@ class CourseInstanceBaseMixin:
             self.url_without_language = remove_query_param_from_url(self.request.get_full_path(), 'hl')
             self.query_language = None
             self.user_language = None
-            self.points = CachedPoints(self.instance, self.request.user, self.content, self.is_course_staff)
+            self.points = CachedPoints(
+                self.instance,
+                self.request.user if not isinstance(self.request.user, GraderUser) else AnonymousUser(),
+                self.content,
+                self.is_course_staff,
+            )
 
             self.note(
                 "course", "instance", "content", "user_course_data", "is_student", "is_assistant",