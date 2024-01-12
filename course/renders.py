4ae35807e9775a561d27d0302fb29085b26a3fa7
Asma Jamil <asma.89.jamil@hotmail.com>
2024-01-12 17:39:54 +0200

    Remove the user profile avatar icons
    This feature is not used and we don't want
    an extra dependency on the gravatar service.
    
    Fixes #698

--- course/renders.py
+++ course/renders.py
@@ -1,18 +1,12 @@
 from django.template import loader
 
 
-def render_avatars(profiles):
-    template = loader.get_template("course/_avatars.html")
-    return template.render({ 'profiles': profiles })
-
-
 def group_info_context(group, profile):
     if not group:
         return { 'id': None }
     return {
         'id': group.id,
-        'collaborators': group.collaborator_names(profile),
-        'avatars': render_avatars(group.members.all()),
+        'collaborators': group.collaborator_names(profile)
     }
 
 
