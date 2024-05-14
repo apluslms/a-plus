0086e01f23d17d0916f6d1e7500985efa7066db7
Jimmy Ihalainen <jimmy.ihalainen@gmail.com>
2024-05-14 13:56:52 +0300

    Increase end-to-end test container start retries

--- e2e_tests/conftest.py
+++ e2e_tests/conftest.py
@@ -13,22 +13,19 @@ def django_server():
     # pylint: disable-next=consider-using-with
     server_process = subprocess.Popen([os.path.join(path, "run_servers.sh")], stdin=subprocess.PIPE)
 
-    # Wait 60 seconds for the server to be ready
-    max_retries = 60
-    retries = 0
-    while retries < max_retries:
+    # Wait 2 minutes for the server to be ready
+    max_retries = 120
+    for _ in range(max_retries):
         try:
             response = requests.get("http://localhost:8000/") # pylint: disable=missing-timeout
             if response.status_code == 200:
+                # Wait a bit more to let the server settle (tests may fail otherwise)
+                time.sleep(10)
                 break
         except requests.ConnectionError:
             pass
 
-        retries += 1
         time.sleep(1)
-
-    # Wait a bit more to let the server settle (tests may fail otherwise)
-    time.sleep(10)
 
     # Run tests
     yield
