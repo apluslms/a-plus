145a78366f4b238104ba14b744fca163957c5cca
mikaelGusse <mikael.gustafsson@aalto.fi>
2024-05-20 12:11:57 +0300

    Fix issues with submission tags

---
+++ exercise/static/exercise/filter_submissions_by_tag.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+	var checked_buttons = []
+    $('button.filter-tag').click(function() {
+        var tagSlug = $(this).data('tagslug');
+
+        if ($(this).find('.glyphicon').hasClass('glyphicon-unchecked')) {
+            $(this).find('.glyphicon').removeClass('glyphicon-unchecked').addClass('glyphicon-check');
+            checked_buttons.push(tagSlug);
+            filterSubmissions(checked_buttons)
+        } else {
+            $(this).find('.glyphicon').removeClass('glyphicon-check').addClass('glyphicon-unchecked');
+            var index = checked_buttons.indexOf(tagSlug);
+            if (index !== -1) {
+                checked_buttons.splice(index, 1);
+            }
+            filterSubmissions(checked_buttons)
+        }
+    });
+
+});
+
+function filterSubmissions(checked_buttons) {
+	let trs = document.getElementsByTagName('table')[0].getElementsByTagName('tr');
+
+	let submissionTags = [];
+
+	for (let i = 0; i < trs.length; i++) {
+		let tagTd = trs[i].getElementsByTagName('td')['submission_tags'];
+
+		if (tagTd) {
+			let parser = new DOMParser();
+			let doc = parser.parseFromString(tagTd.innerHTML, "text/html");
+
+			let tagSlugs = Array.from(doc.getElementsByTagName('span')).map((el) => el.getAttribute('data-tagslug'));
+
+			submissionTags.push(...tagSlugs);
+
+			let checkedButtonsArr = checked_buttons.map(item => item.replace(/;/g, ""));
+
+			if (checkedButtonsArr.length > 0 && !checkedButtonsArr.every(slug => tagSlugs.includes(slug))) {
+				trs[i].style.display = 'none';
+			}
+			else {
+				trs[i].style.display = 'table-row';
+			}
+
+		}
+	}
+};
