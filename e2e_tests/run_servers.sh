bd5d1479640474a41de220d6f9b5a89c272820ba
Jimmy Ihalainen <jimmy.ihalainen@gmail.com>
2025-01-28 09:03:45 +0200

    Improve e2e_tests/run_servers.sh script

--- e2e_tests/run_servers.sh
+++ e2e_tests/run_servers.sh
@@ -2,24 +2,20 @@
 
 export COMPOSE_FILE=docker-compose.yml
 
-scriptpath=$(dirname $(realpath "$0"))
+scriptpath=$(dirname "$(realpath "$0")")
 
 # Move to the directory containing this file
-cd "$scriptpath"
+cd "${scriptpath}" || { echo "Failed to move to directory ${scriptpath}!"; exit 1; }
 
 # Clone aplus-manual if it hasn't been cloned yet
 if ! [ -d ../../aplus-manual ]; then
     git clone https://github.com/apluslms/aplus-manual.git ../../aplus-manual
-    cd ../../aplus-manual
-    git submodule update --init
-    cd ../a-plus/e2e_tests
+    ( cd ../../aplus-manual && git submodule update --init && cd "${scriptpath}" ) || { echo "Failed to initialize and update git submodules!"; exit 1; }
 fi
 
 # Move to aplus-manual directory and build the course if it hasn't been built yet
 if ! [ -d ../../aplus-manual/_build ]; then
-    cd ../../aplus-manual
-    ./docker-compile.sh
-    cd ../a-plus/e2e_tests
+    ( cd ../../aplus-manual && ./docker-compile.sh && cd "${scriptpath}" ) || { echo "Failed to build course!"; exit 1; }
 fi
 
 # Run the server
