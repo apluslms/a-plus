d5b8dae671c2b466d6186f50b655334c72d2f323
jensjvh <43621451+jensjvh@users.noreply.github.com>
2026-06-25 14:19:46 +0300

    Update 403 and 404 page to include breadcrumb and navigation if course detected from url

--- aplus/views.py
+++ aplus/views.py
@@ -1,17 +1,45 @@
+from django.urls import resolve, Resolver404
 from django.shortcuts import render
+from course.models import Course, CourseInstance
+
+def _get_course_instance_from_url(request):
+    try:
+        url_match = resolve(request.path_info)
+        course_slug = url_match.kwargs.get('course_slug')
+        instance_slug = url_match.kwargs.get('instance_slug')
+    except Resolver404:
+        course_slug = None
+        instance_slug = None
+
+    course = Course.objects.filter(url=course_slug).first() if course_slug else None
+    instance = (
+        CourseInstance.objects.filter(course=course, url=instance_slug).first()
+        if course and instance_slug else None
+    )
+
+    return course, instance
+
+def _get_context(request):
+    course, instance = _get_course_instance_from_url(request)
+    base_template = 'course/course_base.html' if instance else 'base.html'
+
+    context = {
+        'show_language_toggle': True,
+        'course': course,
+        'instance': instance,
+        'base_template': base_template
+    }
+
+    return context
 
 # pylint: disable-next=unused-argument
 def error_404(request, exception=None):
-    context = {
-        'show_language_toggle': True,
-    }
+    context = _get_context(request)
     return render(request, '404.html', context, status=404)
 
 # pylint: disable-next=unused-argument
 def error_403(request, exception=None):
-    context = {
-        'show_language_toggle': True,
-    }
+    context = _get_context(request)
     return render(request, '403.html', context, status=403)
 
 # pylint: disable-next=unused-argument
